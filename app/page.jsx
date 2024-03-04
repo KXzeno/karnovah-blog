678d4bc499c3735ad152541983e4401538af9dd8
git <82054121+KXzeno@users.noreply.github.com>
2024-03-04 10:04:44 -0800

    Export Home component on landing page

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,18 @@
 "use client";
 import React from 'react';
 import Image from 'next/image';
+import { Home } from './components';
 
-export default function Home() {
-  let [notice, setNotice] = React.useState("Ongoing Site Reconstruction");
-
-  React.useEffect(() => {
-    const timer = setTimeout(() => {
-      setNotice(prevNotice => prevNotice === "Ongoing Site Reconstruction..."
-        ? "Ongoing Site Reconstruction"
-        : `${prevNotice}.`);
-    }, 500);
-    return () => clearTimeout(timer);
-  }, [notice]);
-
+export default function Blog() {
   const id = React.useId();
   const userId = `${id}-username`;
   const passId = `${id}-password`;
 
   return(
     <>
-      <div className="container min-w-full min-h-screen">
-        <div className="flex flex-auto bg-black min-h-screen">
-          <div className="place-self-center mx-auto animate-bounce hover:animate-spin">
-            <p className="z-10 font-sono text-[2rem] max-sm:text-[1.1rem]">{notice}</p>
-          </div>
-        </div>
+      <Home />
+      <div className="mt-[400rem]">
+        Test
       </div>
     </>
   );
