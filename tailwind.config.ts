93594d497722874f80fb3909327b61fedb389a3a
git <82054121+KXzeno@users.noreply.github.com>
2024-03-04 10:04:29 -0800

    feat: import fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,24 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        urbanist: ['var(--font-urbanist)'],
+        inter: ['var(--font-inter)'],
+        cinzel: ['var(--font-cinzel)'],
+        diphylleia: ['var(--font-diphylleia)'],
+        merriweather: ['var(--font-merriweather)'],
+        spectral: ['var(--font-spectral)'],
+        dm_sans: ['var(--font-dm-sans)'],
+        quicksand: ['var(--font-quicksand)'],
+        dosis: ['var(--font-quicksand)'],
+        sono: ['var(--font-sono)'],
+      },
+      container: {
+        padding: "0.3rem 0.15rem",
+      },
+      colors: {
+        'onyx': '#352F44',
+      },
     },
   },
   plugins: [],
