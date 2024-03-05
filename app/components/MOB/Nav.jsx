03a7a741d8cdf92373174b96bf0318eae1bdc4cc
git <82054121+KXzeno@users.noreply.github.com>
2024-03-05 14:13:13 -0800

    feat: add dynamic routing and submenu

--- app/components/MOB/Nav.jsx
+++ app/components/MOB/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
 import './MOB.css';
+import useToggle from '@H/use-toggle';
 
 export default function Nav() {
   const router = useRouter();
@@ -10,26 +11,85 @@ export default function Nav() {
     '$',
   ]);
 
+  let [showSubMenu, toggleSubMenu] = useToggle(false);
+
+  // const subMenuRef = React.useRef();
+
+  let handleRoute = (e) => {
+    let { target: { outerText: value } } = e;
+    // return value !== '▼' && router.push(`${value.toLowerCase()}`)
+    return value === '▼' ? toggleSubMenu() : router.push(`${value.toLowerCase()}`);
+  }
+
   return (
-    <div id="nav-box" className="bg-[#18122B]">
-      <button type="button" onClick={() => router.push('/recent')}>
-        Recent
-      </button>
-      <button type="button" onClick={() => router.push('/posts')}>
-        Posts<span className="text-[0.7rem] ml-[0.1rem]">▼</span>
-      </button>
-      <button type="button" onClick={() => router.push('/about')}>
-        About
-      </button>
-      <div className={`${dollarSigns != 0 ? 'visible' : 'hidden'} absolute right-8 h-16 w-16`}>
-        <button 
-          className="absolute inset-0 text-center text-inherit"
-          onClick={(event) => {
-          setDollarSigns(dollarSigns.slice(1))
-          }}
-        >
-          { dollarSigns }
+    <>
+      <div id="nav-box">
+        <button type="button" onClick={handleRoute} className="nav-btn">
+          Recent
         </button>
+        <button 
+          type="button" 
+          onClick={handleRoute} 
+          className="relative nav-btn-posts">
+          <p className="nav-btn inline">
+            Posts
+          </p>
+          <span 
+            dir="rtl"
+            // ref={subMenuRef}
+            id="sub-menu"
+          >
+            ▼
+          </span>
+        </button>
+        <button type="button" onClick={handleRoute} className="nav-btn">
+          About
+        </button>
+        <div className={`${dollarSigns != 0 ? 'visible' : 'hidden'} absolute right-8 h-16 w-16`}>
+          <button 
+            className="absolute inset-0 text-center text-inherit"
+            onClick={(event) => {
+              setDollarSigns(dollarSigns.slice(1))
+            }}
+          >
+            { dollarSigns }
+          </button>
+        </div>
+      </div>
+      { showSubMenu && <SubMenu /> }
+    </>
+  );
+}
+
+function SubMenu() {
+  return (
+    <div className="grid grid-cols-2 absolute bg-white text-black w-48 h-64 top-16 justify-self-center inset-0 left-[4rem] text-center text-sm">
+      <div>
+        <p>Test</p>
+      </div>
+      <div>
+        <p>Test</p>
+      </div>
+      <div>
+        <p>Test</p>
+      </div>
+      <div>
+        <p>Test</p>
+      </div>
+      <div>
+        <p>Test</p>
+      </div>
+      <div>
+        <p>Test</p>
+      </div>
+      <div>
+        <p>Test</p>
+      </div>
+      <div>
+        <p>Test</p>
+      </div>
+      <div>
+        <p>Test</p>
       </div>
     </div>
   );
