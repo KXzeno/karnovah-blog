6a8c0ec29f2a25eebbca0ff0197a7445c972eb98
git <82054121+KXzeno@users.noreply.github.com>
2024-03-07 05:41:50 -0800

    Add footer component

---
+++ app/components/MOB/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  <div className="absolute">
+    <div>
+      
+    </div>
+  </div>
+}
--- app/components/index.jsx
+++ app/components/index.jsx
@@ -1,9 +1,11 @@
 import Home from './Home';
 import Nav from '@M/Nav';
 import SubMenu from '@F/SubMenu';
+import Footer from '@M/Footer';
 
 export {
   Home,
   Nav,
   SubMenu,
+  Footer,
 }
