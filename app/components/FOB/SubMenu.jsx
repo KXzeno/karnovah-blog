96d23914a70b00413bb09ccf669ef3494d2f4572
git <82054121+KXzeno@users.noreply.github.com>
2024-03-09 18:47:52 -0800

    fix: readjust absolute positioning of pointer

--- app/components/FOB/SubMenu.jsx
+++ app/components/FOB/SubMenu.jsx
@@ -7,7 +7,7 @@ export default function SubMenu({ children }) {
       <div className="grid grid-cols-2 absolute bg-white text-black w-48 h-min max-h-80 top-16 justify-self-center inset-0 left-[4rem] text-center text-sm place-items-center rounded-[0.7rem] p-2">
         {children}
         <div className="absolute">
-          <div className="z-1 bg-white h-4 w-4 origin-center rotate-45 rounded-tl-[0.1rem] -translate-y-[7.9rem] translate-x-[0.267rem]">
+          <div className="z-2 bg-white h-4 w-4 origin-center rotate-45 rounded-tl-[0.1rem] -translate-y-[1.7rem] translate-x-[0.267rem]">
           </div>
         </div>
       </div>
