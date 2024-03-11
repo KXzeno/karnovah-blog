fab759012ea77d8aadedb0a1163dc5d330608ef6
git <82054121+KXzeno@users.noreply.github.com>
2024-03-11 10:23:45 -0700

    Add click listener custom hook

---
+++ app/components/hooks/use-click-listener.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function useClickListener() {
+  const [mouseClick, setMouseClick] = React.useState(false);
+
+  React.useEffect(() =>
+    function handleMouseClick() {
+      setMouseClick((isClick) => !isClick);
+   
+
+    window.addEventListener('click', handleMouseClick);
+
+    return () => {
+      window.removeEventListener('click', handleMouseClick);
+    };
+  }, []);
+
+  return mouseClick;
+}
