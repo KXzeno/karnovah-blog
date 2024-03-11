fcc59f2619fecb1002f967c2a64f055b8fa4adb0
git <82054121+KXzeno@users.noreply.github.com>
2024-03-10 18:56:13 -0700

    Create footer component with dynamic date render

--- app/components/MOB/Footer.jsx
+++ app/components/MOB/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 export default function Footer() {
-  <div className="absolute">
-    <div>
-      
+  const currYear = new Date().getFullYear();
+
+  return (
+    <div className="absolute inset-x-0 bottom-0 min-w-max h-min">
+      <div className="text-center bg-violet-950 py-2 text-[0.77rem]">
+        <p>Copyright <span className="text-[1rem] align-middle">©</span> {currYear} Karnovah </p>
+      </div>
     </div>
-  </div>
+  );
 }
