1d597b5da68b516b30d1aef4a42f440902419385
git <82054121+KXzeno@users.noreply.github.com>
2024-03-11 10:24:04 -0700

    feat: hide submenu after external click

--- app/components/FOB/SubMenu.jsx
+++ app/components/FOB/SubMenu.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import useClickListener from '@H/use-click-listener';
 
-export default function SubMenu({ children }) {
+export default function SubMenu({ children, showSubMenu }) {
+  let [isClicked, setIsClicked] = React.useState(false);
+  let promptHook = useClickListener();
+
+  // TODO: Unmount hook after clicked out from SubMenu
+  React.useEffect(() => {
+    if (showSubMenu) {
+      promptHook;
+    }
+
+    if (promptHook === true) {
+      setIsClicked((prev) => !prev);
+    }
+
+    return () => {
+      promptHook = undefined;
+    }
+  }, [promptHook]);
 
   return (
-    <>
+    !!isClicked == false
+    ? <>
       <div className="grid grid-cols-2 absolute bg-white text-black w-48 h-min max-h-80 top-16 justify-self-center inset-0 left-[4rem] text-center text-sm place-items-center rounded-[0.7rem] p-2">
         {children}
         <div className="absolute">
@@ -12,5 +31,6 @@ export default function SubMenu({ children }) {
         </div>
       </div>
     </>
+    : undefined
   );
 }
