776a6c37dffeeaf26a9d681b7e48aa0f161728a0
git <82054121+KXzeno@users.noreply.github.com>
2024-03-11 15:17:36 -0700

    feat: use reserve state and hook for autofocus

--- app/components/FOB/SubMenu.jsx
+++ app/components/FOB/SubMenu.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import useClickListener from '@H/use-click-listener';
 
-export default function SubMenu({ children, toggleSubMenu}) {
+export default function SubMenu({ children, toggleSubMenu }) {
   let subRef = React.useRef();
+  let clickListener = useClickListener();
+  let [clicked, setClicked] = React.useState(false);
 
-  let handlePointerLeave = () => {
-    subRef.current = toggleSubMenu();
+  let reserveState = () => {
+    setClicked(true);
   }
 
+  //TODO: Avoid close when "misclicking" within element
+  React.useEffect(() => {
+    console.log(!!toggleSubMenu, clickListener);
+    (!!toggleSubMenu === true && clickListener === true && clicked !== true) ? toggleSubMenu() : undefined;
+
+    return () => setClicked((prev) => false);
+  }, [clickListener]);
+
   return (
-    <div ref={subRef} onPointerLeave={handlePointerLeave}>
+    <div ref={subRef} onPointerLeave={toggleSubMenu} onClick={reserveState}>
       <div className="grid grid-cols-2 absolute bg-white text-black w-48 h-min max-h-80 top-16 justify-self-center inset-0 left-[4rem] text-center text-sm place-items-center rounded-[0.7rem] p-2">
         {children}
         <div className="absolute">
