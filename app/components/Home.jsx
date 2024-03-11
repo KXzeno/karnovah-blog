f26a8849e18375b1942883471f207e0c1493c348
git <82054121+KXzeno@users.noreply.github.com>
2024-03-10 18:55:51 -0700

    Call Footer component

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import Nav from './MOB/Nav';
+import Nav from '@M/Nav';
+import Footer from '@M/Footer';
 
 export default function Home() {
   return (
     <>
       <Nav />
+
+      <Footer />
     </>
   );
 }
