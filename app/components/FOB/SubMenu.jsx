48e14734cbbb49e6a6cd129c857373719ecb20d3
git <82054121+KXzeno@users.noreply.github.com>
2024-03-12 16:27:28 -0700

    fix: grant mobile intuitive display obfuscation
    Used window class to get screen size, and make conditional functions
    with different behaviors as pointers work differently betwen PC and
    mobile

--- app/components/FOB/SubMenu.jsx
+++ app/components/FOB/SubMenu.jsx
@@ -9,38 +9,81 @@ export default React.forwardRef(function SubMenu({
 }, subMenuRef) {
   let [checkChange, setCheckChange] = React.useState(1);
 
-  function toggleEnter() {
+  function toggleEnter(e) {
     setCheckChange((prev) => prev + 1);
   }
 
-  function toggleLeave() {
+  function toggleLeave(e) {
+    if (e.target === subMenuRef.current.firstChild) {
+      return;
+    }
+
+    if (isMobile) {
+      toggleSubMenu();
+    }
+
     setCheckChange(0);
   }
 
   let [pointer, setPointer] = useClickListener();
 
+  let [width, setWidth] = React.useState(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+
   React.useEffect(() => {
-    subMenuRef.current.addEventListener('pointermove', toggleEnter)
-    subMenuRef.current.addEventListener('pointerleave', toggleLeave)
-    showSubMenu === true && pointer === true && checkChange !== 0
-      ? +(() => {
-        setPointer(false);
-        return;
-      })()
-      : +(() => {
-        if (checkChange === 0 && pointer === true && showSubMenu === true) {
-          toggleSubMenu();
-        }
-      })();
+    window.addEventListener('resize', handleWindowSizeChange);
     return () => {
-      removeEventListener('pointermove', toggleEnter);
-      removeEventListener('pointerleave', toggleLeave);
+      window.removeEventListener('resize', handleWindowSizeChange);
     }
-    console.log(showSubMenu, !!subMenuRef.current);
+  }, []);
+
+  let isMobile = width <= 768;
+
+  React.useEffect(() => {
+    console.log(showSubMenu, pointer);
+
+    if (!isMobile) {
+      subMenuRef.current.addEventListener('pointermove', toggleEnter);
+      subMenuRef.current.addEventListener('pointerleave', toggleLeave);
+
+      showSubMenu === true && pointer === true && checkChange !== 0
+        ? +(() => {
+          setPointer(false);
+          return;
+        })()
+        : +(() => {
+          if (checkChange === 0 && pointer === true && showSubMenu === true) {
+            toggleSubMenu();
+          }
+        })();
+      return () => {
+        subMenuRef.current?.removeEventListener('pointermove', toggleEnter);
+        subMenuRef.current?.removeEventListener('pointerleave', toggleLeave);
+      }
+    }
+
+    if (isMobile) {
+      // showSubMenu === true && isMobile && checkChange !== 0
+      subMenuRef.current.addEventListener('touchend', toggleEnter);
+      window.addEventListener('touchend', toggleLeave);
+
+      showSubMenu === true && checkChange !== 0
+        ? setCheckChange(1)
+        : toggleSubMenu();
+
+      return () => {
+        subMenuRef.current?.removeEventListener('touchend', toggleEnter);
+        window.removeEventListener('touchend', toggleLeave);
+      }
+    }
+
   }, [pointer, showSubMenu, toggleSubMenu]);
 
   return (
-    <div ref={subMenuRef} className="pointer-events-auto">
+    <div ref={subMenuRef} className="pointer-events-auto z-10">
       <div className="grid grid-cols-2 absolute bg-white text-black w-48 h-min max-h-80 top-16 justify-self-center inset-0 left-[4rem] text-center text-sm place-items-center rounded-[0.7rem] p-2">
         {children}
         {checkChange}
