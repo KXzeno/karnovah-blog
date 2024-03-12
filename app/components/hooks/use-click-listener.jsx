4950611e711d47502e053f1d8c316681bccc561b
git <82054121+KXzeno@users.noreply.github.com>
2024-03-12 00:51:25 -0700

    FIX!: change event and properly use callback arg
    Deployment did not work due to useEffect improper syntax, only worked
    locally from strict mode's 2-render.

--- app/components/hooks/use-click-listener.jsx
+++ app/components/hooks/use-click-listener.jsx
@@ -3,17 +3,19 @@ import React from 'react';
 export default function useClickListener() {
   const [mouseClick, setMouseClick] = React.useState(false);
 
-  React.useEffect(() =>
-    function handleMouseClick() {
+  React.useEffect(() => {
+    function handlePointerDown() {
       setMouseClick((isClick) => !isClick);
+    }
 
+    document.addEventListener('pointerdown', handlePointerDown);
+    console.log("Event fired");
 
-      window.addEventListener('click', handleMouseClick);
-
-      return () => {
-        window.removeEventListener('click', handleMouseClick);
-      };
-    }, []);
+    return () => {
+      document.removeEventListener('pointerdown', handlePointerDown);
+      console.log("Event closed");
+    };
+  }, []);
 
   return mouseClick;
 }
