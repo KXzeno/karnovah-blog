c730e978dcdff2adb003e4d640a0ba68d8fc872c
git <82054121+KXzeno@users.noreply.github.com>
2024-03-12 14:52:34 -0700

    feat: use ref hook for better event handling

--- app/components/FOB/SubMenu.jsx
+++ app/components/FOB/SubMenu.jsx
@@ -1,25 +1,49 @@
 import React from 'react';
 import useClickListener from '@H/use-click-listener';
 
-export default function SubMenu({ 
+export default React.forwardRef(function SubMenu({ 
   children, 
   showSubMenu, 
   toggleSubMenu, 
   ...delegated
-}) {
+}, subMenuRef) {
+  let [checkChange, setCheckChange] = React.useState(1);
 
-  let clickListener = useClickListener();
+  function toggleEnter() {
+    setCheckChange((prev) => prev + 1);
+  }
+
+  function toggleLeave() {
+    setCheckChange(0);
+  }
+
+  let [pointer, setPointer] = useClickListener();
+
   React.useEffect(() => {
-    if (showSubMenu === true && clickListener === true) {
-      toggleSubMenu();
+    subMenuRef.current.addEventListener('pointermove', toggleEnter)
+    subMenuRef.current.addEventListener('pointerleave', toggleLeave)
+    showSubMenu === true && pointer === true && checkChange !== 0
+      ? +(() => {
+        setPointer(false);
+        return;
+      })()
+      : +(() => {
+        if (checkChange === 0 && pointer === true && showSubMenu === true) {
+          toggleSubMenu();
+        }
+      })();
+    return () => {
+      removeEventListener('pointermove', toggleEnter);
+      removeEventListener('pointerleave', toggleLeave);
     }
-    console.log(showSubMenu, clickListener);
-  }, [clickListener, showSubMenu, toggleSubMenu]);
+    console.log(showSubMenu, !!subMenuRef.current);
+  }, [pointer, showSubMenu, toggleSubMenu]);
 
   return (
-    <div>
+    <div ref={subMenuRef} className="pointer-events-auto">
       <div className="grid grid-cols-2 absolute bg-white text-black w-48 h-min max-h-80 top-16 justify-self-center inset-0 left-[4rem] text-center text-sm place-items-center rounded-[0.7rem] p-2">
         {children}
+        {checkChange}
         <div className="absolute">
           <div className="z-2 bg-white h-4 w-4 origin-center rotate-45 rounded-tl-[0.1rem] -translate-y-[1.7rem] translate-x-[0.267rem]">
           </div>
@@ -27,4 +51,4 @@ export default function SubMenu({
       </div>
     </div>
   );
-}
+})
