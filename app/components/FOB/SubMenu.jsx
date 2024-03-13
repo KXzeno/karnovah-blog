fcff435cfc4fa10ee7a7e2edb5e79bfc87d90c2c
git <82054121+KXzeno@users.noreply.github.com>
2024-03-12 18:19:00 -0700

    fix: transform element upwards for visibility

--- app/components/FOB/SubMenu.jsx
+++ app/components/FOB/SubMenu.jsx
@@ -126,12 +126,12 @@ export default React.forwardRef(function SubMenu({
   }, [pointer, showSubMenu, toggleSubMenu]);
 
   return (
-    <div ref={subMenuRef} className="pointer-events-auto z-10">
+    <div ref={subMenuRef} className="pointer-events-auto">
       <div className="grid grid-cols-2 absolute bg-white text-black w-48 h-min max-h-80 top-16 justify-self-center inset-0 left-[4rem] text-center text-sm place-items-center rounded-[0.7rem] p-2">
         {children}
         {checkChange}
         <div className="absolute">
-          <div className="z-2 bg-white h-4 w-4 origin-center rotate-45 rounded-tl-[0.1rem] -translate-y-[1.7rem] translate-x-[0.267rem]">
+          <div className="z-2 bg-white h-4 w-4 origin-center rotate-45 rounded-tl-[0.1rem] -translate-y-[2.3rem] translate-x-[0.267rem]">
           </div>
         </div>
       </div>
