bd0767ce0c9d1e98a9ed24f317c897d5eaac84ab
git <82054121+KXzeno@users.noreply.github.com>
2024-03-19 13:01:37 -0700

    feat: handle dynamic metadata

---
+++ app/posts/[slug]/page.jsx
@@ -0,0 +1,21 @@
+export async function generateMetadata({ params, searchParams }, parent) {
+  // read route params
+  const { slug } = params;
+
+  // fetch data
+  const post = await fetch(`https://blog.karnovah.com/posts/${slug}`).then((res) => res.json());
+
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: `${post.title}`,
+    /*
+    openGraph: {
+      images: ['/path/to/img', ...previousImages],
+    },
+    */
+  }
+}
+
+export default function Page({ params, searchParams }) {}
