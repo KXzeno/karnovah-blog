bdfa27b0c2062f41a35307c9fc1c7ac57d87a860
git <82054121+KXzeno@users.noreply.github.com>
2024-03-19 19:24:15 -0700

    feat: add warning msg display

--- app/posts/akhundelar/page.jsx
+++ app/posts/akhundelar/page.jsx
@@ -15,49 +15,69 @@ export default function Akhundelar() {
     <>
       <Nav />
       <div className="body-layout">
-          <article className="post">
-            <h1>Akhundelar</h1>
-            <hgroup className="sub-heading">
-              <div>
-                <h2>We are all born with spears, but most are lead to think it is only gratifying when thrown.</h2>  
-              </div>
-              <aside>
-                <h4>
-                  This post is primarily made to find my way of dealing with <abbr title="Search Engine Optimization">SEO</abbr> and understanding my design system plus content flow.
-                </h4>
-              </aside>
-            </hgroup>
+        <article className="post">
+          <h1>Akhundelar</h1>
+          <hgroup className="sub-heading">
             <div>
-              <section>
-                <h3>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel ex euismod, varius diam sed, iaculis lectus. Curabitur vitae eros in odio pellentesque viverra vitae eget mi. Donec ultricies eros sapien, quis vehicula risus dignissim sit amet. Quisque interdum placerat dolor. Aliquam sem erat, tristique aliquet nibh id, convallis dictum neque. Cras mattis sed ligula vitae fermentum. Etiam convallis ipsum et felis mollis, ut pretium dolor laoreet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse dignissim, mi eget elementum posuere, lorem justo bibendum nulla, et mattis erat tortor ac felis. Vestibulum eget vehicula velit. Aliquam scelerisque magna ac lobortis semper. Donec sed risus ligula.
-
-Quisque ullamcorper enim in quam ullamcorper consectetur. Phasellus consequat nisl tortor, sit amet consectetur mi blandit tempor. Quisque aliquam nec lacus vel faucibus. In consequat, ante sit amet vulputate lobortis, lorem sapien suscipit mi, ut varius dolor mauris eu ante. Aliquam vel scelerisque ipsum. Nulla facilisi. Sed placerat ipsum tempus arcu sollicitudin, id aliquam ligula fringilla. Nam at dapibus urna, maximus feugiat urna. Donec sit amet odio quis nibh elementum lacinia. Donec ullamcorper volutpat erat, id consequat dui vulputate at. Praesent dolor nunc, egestas eu ex a, semper feugiat diam. Cras cursus ipsum massa, ut vehicula purus placerat quis.
-
-Cras dictum, nisi at posuere finibus, nulla lacus sollicitudin erat, vel vulputate sapien purus ac lorem. Quisque ac elit mattis tellus pharetra viverra. Proin blandit turpis facilisis metus porttitor, vel suscipit ligula pellentesque. Quisque porttitor orci non ligula eleifend, vel lobortis enim faucibus. Praesent et metus sit amet massa varius volutpat. Nunc feugiat, risus et eleifend suscipit, odio nisl consectetur augue, nec iaculis metus tellus ac lacus. Donec nec arcu nisl. Aenean felis diam, sollicitudin non sem id, viverra tristique purus. Nulla sit amet lectus tempus, suscipit dolor at, finibus eros.
-
-Fusce volutpat consequat enim at consequat. Nam dui quam, condimentum sit amet consequat sed, fringilla et turpis. Praesent sed sagittis enim. Etiam vel nunc sem. Aenean massa velit, pulvinar ut ullamcorper nec, porttitor a dui. Ut varius blandit viverra. Etiam sodales sed mi vitae eleifend.
-
-Integer non venenatis odio, in interdum nunc. In pharetra enim in lorem fermentum, eget posuere lectus blandit. Sed ornare lacinia lectus, laoreet interdum ante auctor non. Donec commodo accumsan lacinia. Aliquam felis nulla, sollicitudin non pharetra accumsan, pellentesque a velit. In a placerat enim, eleifend iaculis nisi. Etiam non velit euismod, semper arcu eget, lacinia ante. Vestibulum purus ipsum, auctor non eleifend id, rutrum at ex. Cras eget viverra sapien, quis maximus eros. Donec elementum dui vitae lectus eleifend, in auctor nibh malesuada. Donec facilisis maximus dolor sed euismod. Duis pretium nisl augue, eu elementum diam consectetur vel.
-
-Mauris euismod tellus vitae ipsum dignissim, vitae cursus odio sodales. Vestibulum sit amet mi est. Maecenas rhoncus at neque vel placerat. Ut elementum sem nulla, vel fermentum diam bibendum non. Etiam vel orci mi. Phasellus lobortis orci nulla, at sollicitudin nibh fermentum quis. Donec venenatis nec ligula et vulputate. Ut vel nibh tempor, maximus est sit amet, fringilla orci. Duis tempor venenatis faucibus. In maximus consectetur ipsum eget sodales. Curabitur elementum molestie arcu, id rhoncus libero eleifend pulvinar. Nullam aliquet, mauris non porta luctus, dolor diam ultrices nisi, at porta felis magna vel quam. Sed eu dui varius, eleifend risus non, accumsan nisl. Aliquam purus enim, volutpat sit amet quam eget, consectetur luctus sem.
-
-Fusce viverra auctor ullamcorper. Vestibulum tincidunt erat sit amet facilisis elementum. Ut mattis neque turpis, nec imperdiet orci facilisis vitae. Vivamus laoreet tincidunt libero, semper faucibus quam. Nam ac lacus quis risus consequat eleifend in sit amet nisi. Etiam suscipit augue ex, ultricies pellentesque ligula posuere vitae. Donec non lorem aliquam, interdum magna non, tincidunt metus. Fusce eget augue euismod, molestie felis cursus, facilisis metus. Mauris rhoncus, ante pharetra congue dictum, lectus nisi vulputate dui, at dignissim orci ipsum in nunc.
-
-Maecenas non tempus est, eget pellentesque augue. Proin iaculis ut enim sit amet congue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus imperdiet porta dolor, vel eleifend ipsum aliquet vitae. Morbi feugiat, turpis pretium porttitor interdum, magna diam fringilla nisl, quis gravida turpis metus vitae dui. Praesent eleifend neque quis laoreet aliquam. Duis tempor tincidunt ligula, eget condimentum erat interdum at. Nullam vestibulum finibus est, quis hendrerit tortor tincidunt eu. Suspendisse ut sapien faucibus, molestie tellus at, lacinia elit. Duis interdum malesuada augue vitae pulvinar. Quisque viverra dictum urna eu consectetur. Fusce sodales viverra quam.
-
-In nisl ligula, dignissim eu ultricies vitae, egestas sit amet augue. Phasellus varius massa eros, ut ullamcorper nisi dapibus eget. Nulla rutrum lobortis neque a hendrerit. Nulla sed neque eget libero vehicula pulvinar at ut lorem. Pellentesque tristique, neque in facilisis iaculis, lorem sapien imperdiet ante, a mollis velit quam id elit. Duis orci tellus, euismod et arcu ac, vulputate iaculis nunc. Aliquam lacus sapien, interdum sed purus et, ultricies mollis enim. Vivamus ut quam id urna rhoncus condimentum nec in risus. In rhoncus vehicula libero eu accumsan. Nam sit amet ex id mi aliquet auctor. Sed condimentum justo sit amet condimentum pharetra. Proin ornare nulla non porta eleifend.
-
-Proin ac lacus tortor. Donec sit amet velit vel massa tincidunt mollis venenatis vitae ligula. Proin pretium erat quam, vel placerat orci efficitur quis. Nunc ullamcorper placerat arcu, id dignissim nisi scelerisque a. Sed molestie, justo et tincidunt consequat, leo ex dictum lectus, ac egestas augue risus id nulla. Aenean laoreet tincidunt malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id nisi eleifend, porttitor ante ut, fermentum turpis. Donec faucibus dolor et tortor maximus lobortis. Fusce scelerisque gravida euismod. Ut et luctus ipsum. Curabitur aliquet posuere dui quis gravida.
-                </h3>
-              </section>
+              <h2>We are all born with spears, but most are lead to think it is only gratifying when thrown.</h2>  
             </div>
-          </article>
+            <aside className="relative bg-[#232D3F] rounded-[2px] p-4 m-2 rounded-tl-[11px] border-l-2 border-yellow-500">
+              <h4>
+                This post is primarily made to find my way of dealing with <abbr title="Search Engine Optimization">SEO</abbr> and understanding my design system plus content flow.
+              </h4>
+              <div className="w-8 h-8 z-10 bg-black absolute inline-flex top-0 left-0 absolute -translate-x-[0.8rem] -translate-y-[0.8rem] rounded-br-[1.3rem]">
+                <svg
+                  className="h-6 w-6 text-yellow-500 -translate-y-[3px]"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z" />
+                  <path d="M12 9v4" />
+                  <path d="M12 17h.01" />
+                </svg>
+              </div>
+            </aside>
+          </hgroup>
+          <div>
+            <section className="primary-content">
+              <div>
+                <h3>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel ex euismod, varius diam sed, iaculis lectus. Curabitur vitae eros in odio pellentesque viverra vitae eget mi. Donec ultricies eros sapien, quis vehicula risus dignissim sit amet. Quisque interdum placerat dolor. Aliquam sem erat, tristique aliquet nibh id, convallis dictum neque. Cras mattis sed ligula vitae fermentum. Etiam convallis ipsum et felis mollis, ut pretium dolor laoreet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse dignissim, mi eget elementum posuere, lorem justo bibendum nulla, et mattis erat tortor ac felis. Vestibulum eget vehicula velit. Aliquam scelerisque magna ac lobortis semper. Donec sed risus ligula.
+
+                  Quisque ullamcorper enim in quam ullamcorper consectetur. Phasellus consequat nisl tortor, sit amet consectetur mi blandit tempor. Quisque aliquam nec lacus vel faucibus. In consequat, ante sit amet vulputate lobortis, lorem sapien suscipit mi, ut varius dolor mauris eu ante. Aliquam vel scelerisque ipsum. Nulla facilisi. Sed placerat ipsum tempus arcu sollicitudin, id aliquam ligula fringilla. Nam at dapibus urna, maximus feugiat urna. Donec sit amet odio quis nibh elementum lacinia. Donec ullamcorper volutpat erat, id consequat dui vulputate at. Praesent dolor nunc, egestas eu ex a, semper feugiat diam. Cras cursus ipsum massa, ut vehicula purus placerat quis.
+
+                  Cras dictum, nisi at posuere finibus, nulla lacus sollicitudin erat, vel vulputate sapien purus ac lorem. Quisque ac elit mattis tellus pharetra viverra. Proin blandit turpis facilisis metus porttitor, vel suscipit ligula pellentesque. Quisque porttitor orci non ligula eleifend, vel lobortis enim faucibus. Praesent et metus sit amet massa varius volutpat. Nunc feugiat, risus et eleifend suscipit, odio nisl consectetur augue, nec iaculis metus tellus ac lacus. Donec nec arcu nisl. Aenean felis diam, sollicitudin non sem id, viverra tristique purus. Nulla sit amet lectus tempus, suscipit dolor at, finibus eros.
+
+                  Fusce volutpat consequat enim at consequat. Nam dui quam, condimentum sit amet consequat sed, fringilla et turpis. Praesent sed sagittis enim. Etiam vel nunc sem. Aenean massa velit, pulvinar ut ullamcorper nec, porttitor a dui. Ut varius blandit viverra. Etiam sodales sed mi vitae eleifend.
+
+                  Integer non venenatis odio, in interdum nunc. In pharetra enim in lorem fermentum, eget posuere lectus blandit. Sed ornare lacinia lectus, laoreet interdum ante auctor non. Donec commodo accumsan lacinia. Aliquam felis nulla, sollicitudin non pharetra accumsan, pellentesque a velit. In a placerat enim, eleifend iaculis nisi. Etiam non velit euismod, semper arcu eget, lacinia ante. Vestibulum purus ipsum, auctor non eleifend id, rutrum at ex. Cras eget viverra sapien, quis maximus eros. Donec elementum dui vitae lectus eleifend, in auctor nibh malesuada. Donec facilisis maximus dolor sed euismod. Duis pretium nisl augue, eu elementum diam consectetur vel.
+
+                  Mauris euismod tellus vitae ipsum dignissim, vitae cursus odio sodales. Vestibulum sit amet mi est. Maecenas rhoncus at neque vel placerat. Ut elementum sem nulla, vel fermentum diam bibendum non. Etiam vel orci mi. Phasellus lobortis orci nulla, at sollicitudin nibh fermentum quis. Donec venenatis nec ligula et vulputate. Ut vel nibh tempor, maximus est sit amet, fringilla orci. Duis tempor venenatis faucibus. In maximus consectetur ipsum eget sodales. Curabitur elementum molestie arcu, id rhoncus libero eleifend pulvinar. Nullam aliquet, mauris non porta luctus, dolor diam ultrices nisi, at porta felis magna vel quam. Sed eu dui varius, eleifend risus non, accumsan nisl. Aliquam purus enim, volutpat sit amet quam eget, consectetur luctus sem.
+
+                  Fusce viverra auctor ullamcorper. Vestibulum tincidunt erat sit amet facilisis elementum. Ut mattis neque turpis, nec imperdiet orci facilisis vitae. Vivamus laoreet tincidunt libero, semper faucibus quam. Nam ac lacus quis risus consequat eleifend in sit amet nisi. Etiam suscipit augue ex, ultricies pellentesque ligula posuere vitae. Donec non lorem aliquam, interdum magna non, tincidunt metus. Fusce eget augue euismod, molestie felis cursus, facilisis metus. Mauris rhoncus, ante pharetra congue dictum, lectus nisi vulputate dui, at dignissim orci ipsum in nunc.
+
+                  Maecenas non tempus est, eget pellentesque augue. Proin iaculis ut enim sit amet congue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus imperdiet porta dolor, vel eleifend ipsum aliquet vitae. Morbi feugiat, turpis pretium porttitor interdum, magna diam fringilla nisl, quis gravida turpis metus vitae dui. Praesent eleifend neque quis laoreet aliquam. Duis tempor tincidunt ligula, eget condimentum erat interdum at. Nullam vestibulum finibus est, quis hendrerit tortor tincidunt eu. Suspendisse ut sapien faucibus, molestie tellus at, lacinia elit. Duis interdum malesuada augue vitae pulvinar. Quisque viverra dictum urna eu consectetur. Fusce sodales viverra quam.
+
+                  In nisl ligula, dignissim eu ultricies vitae, egestas sit amet augue. Phasellus varius massa eros, ut ullamcorper nisi dapibus eget. Nulla rutrum lobortis neque a hendrerit. Nulla sed neque eget libero vehicula pulvinar at ut lorem. Pellentesque tristique, neque in facilisis iaculis, lorem sapien imperdiet ante, a mollis velit quam id elit. Duis orci tellus, euismod et arcu ac, vulputate iaculis nunc. Aliquam lacus sapien, interdum sed purus et, ultricies mollis enim. Vivamus ut quam id urna rhoncus condimentum nec in risus. In rhoncus vehicula libero eu accumsan. Nam sit amet ex id mi aliquet auctor. Sed condimentum justo sit amet condimentum pharetra. Proin ornare nulla non porta eleifend.
+
+                  Proin ac lacus tortor. Donec sit amet velit vel massa tincidunt mollis venenatis vitae ligula. Proin pretium erat quam, vel placerat orci efficitur quis. Nunc ullamcorper placerat arcu, id dignissim nisi scelerisque a. Sed molestie, justo et tincidunt consequat, leo ex dictum lectus, ac egestas augue risus id nulla. Aenean laoreet tincidunt malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id nisi eleifend, porttitor ante ut, fermentum turpis. Donec faucibus dolor et tortor maximus lobortis. Fusce scelerisque gravida euismod. Ut et luctus ipsum. Curabitur aliquet posuere dui quis gravida.
+                </h3>
+              </div>
+            </section>
+          </div>
+        </article>
         <div className="right-margin">
           <span> Test </span> <span> Test </span> <span> Test </span> <span> Test </span> <span> Test </span> <span> Test </span> <span> Test </span> <span> Test </span> <span> Test </span> <span> Test </span> <span> Test </span>
         </div>
       </div>
       <Footer />
     </>
-  )
+)
 }
