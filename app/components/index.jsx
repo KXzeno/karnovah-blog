8116a4f3f7b4936ca14acd8de2b2f19dca731bca
git <82054121+KXzeno@users.noreply.github.com>
2024-03-20 18:29:37 -0700

    Add new component

--- app/components/index.jsx
+++ app/components/index.jsx
@@ -2,10 +2,12 @@ import Home from './Home';
 import Nav from '@M/Nav';
 import SubMenu from '@F/SubMenu';
 import Footer from '@M/Footer';
+import Icons, { Warning } from '@M/Icons';
 
 export {
   Home,
   Nav,
   SubMenu,
   Footer,
+  Icons, Warning,
 }
