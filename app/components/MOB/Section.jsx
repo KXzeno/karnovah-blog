05534009f8b1b69f1303fc68cfb5218f9f36d67d
git <82054121+KXzeno@users.noreply.github.com>
2024-03-21 21:17:00 -0700

    feat: create polymorphing header component

---
+++ app/components/MOB/Section.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const h7 = Symbol.for('k');
+const h8 = Symbol('k');
+const h9 = Symbol('f');
+const h10 = Symbol.keyFor(h8);
+
+const VALID_SECTIONS = {
+  'sec': 'h4',
+  'subsec': 'h5', 
+  'body': 'h6',
+  [h7]: 'x',
+};
+
+VALID_SECTIONS[h8] = 'z';
+
+export default function Section({
+  children,
+  as: Section = 'sec',
+  ...delegated
+}) {
+  let id = React.useId();
+
+  if (!Object.keys(VALID_SECTIONS).includes(Section)) {
+    throw new Error(`Unrecognized section: ${Section}. Expected: ${VALID_SECTIONS}`);
+  };
+
+  Section = Object.getOwnPropertyDescriptor(VALID_SECTIONS, Section).value;
+
+  return (
+    <Section
+      id={id}
+      {...delegated}
+    >
+      {children}
+    </Section>
+  );
+}
+
