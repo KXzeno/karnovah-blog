b96f0495825d1449915470f8e5ee6b6ff986da48
git <82054121+KXzeno@users.noreply.github.com>
2024-03-24 07:35:36 -0700

    FEAT!: create methods for observer effect

--- app/components/MOB/Section.jsx
+++ app/components/MOB/Section.jsx
@@ -82,29 +82,104 @@ export default React.memo(React.forwardRef(function Section({
     // }));
     console.log(secRef.current);
   });
+  //: +(() => {
+  //  console.log(document.getElementsByClassName('curr-head').length);
+  //})()
+let Overlord = {
+  getName(name) {
+    this.element = document.getElementsByName(name)[0];
+    return this;
+  },
 
-  if (onScreen && secRef.current.id) {
-    console.log(secRef.current.id) 
+  getClass(className) {
+    this.element = document.getElementsByClassName(className); 
+    return this.element;
+  },
+
+  isUnary(className) {
+    this.element = document.getElementsByClassName(className);
+    return this.element.length > 1;
+  },
+
+  setClass(className) {
+    this.element.setAttribute('class', className);
+    return this;
+  },
+
+  rmClass(className) {
+    this.element.removeAttribute('class', className);
+    return this;
+  },
+  // TODO: Fix no attr when fast scroll up
+  // Assign classes to the H4 anchors
+  isolateClass(className) {
+    this.element = document.getElementsByClassName(className);
+    let length = document.getElementsByClassName(className).length;
+    if (length > 1) {
+      for (let i = 0; i < length - 1 ; i++) {
+        let [curr, interceptor] = [Number(this.element.item(i).getAttribute('name').at(2)), 0];
+        console.log(curr, this.element.item(i)?.getAttribute('class'), this.element.item(i+1)?.getAttribute('class'));
+        (this.element.item(i).getAttribute('class') 
+          === undefined)
+          ? (() => {
+            this.element.item(i + 1)?.removeAttribute('class', className);
+        })()
+          : (() => {
+            this.element.item(i)?.removeAttribute('class', className);;
+          })();
+      }
+    }
   }
+}
 
-  // TODO: Use ToC dynamic styling which intercepts other ToC items when in same view
-  return (
-    <SectionContext.Provider
-      value={{ refs, setRefs }}
+let selectorToggle = 'curr-head';
+
+function seekOnScreen(ref, target) {
+  let rank = (onScreen && ref.current?.id) 
+    ? 1
+    : !!(onScreen === false && !!ref.current?.id === true) 
+      ? 2 
+      : 3;
+
+  return rank;
+}
+
+switch (seekOnScreen(secRef, selectorToggle)) {
+  case 1:
+    Overlord.getName(secRef.current?.id).setClass(selectorToggle);
+    break;
+  case 2:
+    Overlord.getName(secRef.current?.id).rmClass(selectorToggle);
+    break;
+  case 3:
+    Overlord.isolateClass(selectorToggle);
+    break;
+  default:
+    console.error(`Error, ref: ${secRef}`);
+}
+
+// (onScreen && secRef.current.id)  
+// ? Overlord.isUnary('curr-head')
+//   ? Overlord.getName(secRef.current.id).setClass('curr-head underline underline-offset-[5px]')
+//   : !!(!!onScreen === false && !!secRef.current?.id === true)
+//   ? Overlord.getName(secRef.current?.id).rmClass('curr-head underline underline-offset-[5px]')
+//   : null;
+
+
+// TODO: Use ToC dynamic styling which intercepts other ToC items when in same view
+return (
+  <SectionContext.Provider
+    value={{ refs, onScreen }}
+  >
+    <Section
+      id={idInserter()}
+      // onLoad={() => console.log(generateRef)}
+      ref={secRef}
+      {...delegated}
     >
-      <Section
-        id={idInserter()}
-        //ref={() => refInserter()}
-        onLoad={() => console.log(generateRef)}
-        ref={secRef}
-        //ref={() => generateRef() ?? catchRef}
-        //ref={refs[refIndex]?.secRef ?? secRef}
-        className={onScreen && secRef.current.id && ''}
-        {...delegated}
-      >
-        {children}
-      </Section>
-    </SectionContext.Provider>
-  );
+      {children}
+    </Section>
+  </SectionContext.Provider>
+);
 }));
 
