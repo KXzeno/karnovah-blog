012ae3864b00373ac5683aa7d28463e0e9f92d84
git <82054121+KXzeno@users.noreply.github.com>
2024-03-25 19:25:42 -0700

    feat: dynamically observe related sections

--- app/components/hooks/useOnscreen.jsx
+++ app/components/hooks/useOnscreen.jsx
@@ -10,14 +10,31 @@ export default function useOnscreen() { // Can instead use elementRef as prop an
 
   //TODO: Use rect to automate intersection of root/element
   React.useEffect(() => {
-    let rect = elementRef.current.getBoundingClientRect();
-    console.log(rect);
+    let [rect, sib] = 
+      [elementRef.current.getBoundingClientRect(), 
+      elementRef.current.nextElementSibling];
+
+    let sibTag = sib ? sib.tagName : null;
+    let sibRect = sibTag !== null ? sib.getBoundingClientRect() : 0;
+
+    let totalHeight = sibRect !== 0 
+      ? rect.height + sibRect.height
+      : rect.height
+
+
+//    let xHeight = (sibTag === 'SECTION')
+ //     ? sib
 
     const observer = new IntersectionObserver((entries) => {
       const [entry] = entries;
       setIsOnscreen(entry.isIntersecting);
-    }, { threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], rootMargin: `-${rect.height}px 100%` });
-    observer.observe(elementRef.current);
+      // console.log(entry.rootBounds, entry.boundingClientRect)
+    }, { threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], rootMargin: `${0}px 0px` });
+    let nextElement = (sib && sib.tagName === 'SECTION') 
+      ? sib : elementRef.current;
+
+    console.log(nextElement);
+    observer.observe(nextElement);
     return () => {
       observer.disconnect();
     };
