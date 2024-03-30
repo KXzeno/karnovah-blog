cfdeef1490e39dacc07d064e338fdccc8595a9ea
git <82054121+KXzeno@users.noreply.github.com>
2024-03-30 00:19:23 -0700

    feat: add responsiveness to footer

--- app/components/MOB/Footer.jsx
+++ app/components/MOB/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function Footer() {
   const currYear = new Date().getFullYear();
 
   return (
-    <footer className="flex flex-row basis-1/6 relative place-items-center h-32 bg-gradient-to-b from-violet-950 to-indigo-900 basis mt-[2.3rem]">
+    <footer className="flex flex-row basis-1/6 relative place-items-center h-32 bg-gradient-to-b from-violet-950 to-indigo-900 basis mt-[2.3rem] text-[#3f7300]">
       <div className="order-0 size-full text-center py-2 text-[0.77rem]">
         <div className="flex size-full place-items-center">
           <p className="inline-block grow">Copyright <span className="text-[1rem] align-middle">©</span> {currYear} Karnovah </p>
@@ -12,18 +13,17 @@ export default function Footer() {
       </div> 
       <div className="grow-0 h-full bg-black bg-gradient-to-r from-violet-950 via-slate-950 to-violet-950 w-2">
       </div>
-      <div className="size-full text-center pt-4">
-        <div className="pb-2">
+      <nav className="size-full text-center pt-4">
+        <div className="pb-2 underline text-[.83rem]">
           Links
         </div>
-        <div className="grid grid-rows-none grid-cols-2 mx-auto w-max">
-          <p>1</p>
-          <p>22</p>
-          <p>333</p>
-          <p>4444</p>
-          <p>55555</p>
+        <div className="grid grid-rows-none grid-cols-3 gap-x-4 gap-y-2 mx-auto w-max text-[0.77rem]">
+          <Link className="col-span-3 hover:text-[#69c000]" href="https://karnovah.com">Apex Domain</Link>
+          <Link className="hover:text-[#69c000]" href="https://github.com/KXzeno">Github</Link>
+          <Link className="hover:text-[#69c000]" href="mailto:karnovah@outlook.com">Contact</Link>
+          <Link className="hover:text-[#69c000]" href="https://discord.gg/5t77NmRYS8">Discord</Link>
         </div>
-      </div>
+      </nav>
     </footer>
   );
 }
