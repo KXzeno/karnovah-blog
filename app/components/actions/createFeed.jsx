7ce964460e5333fa63524e47fd9122dec891eb5a
git <82054121+KXzeno@users.noreply.github.com>
2024-03-31 06:13:22 -0700

    Create server action for internal data fetch

---
+++ app/components/actions/createFeed.jsx
@@ -0,0 +1,26 @@
+'use client';
+import React from 'react';
+
+'use server';
+export async function createFeedLeed(postData) {
+    let postData = {
+      header: post.get('header'),
+    }
+
+    try {
+      const res = await fetch('/akhundelar')
+      if (!res.ok) {
+        throw new Error('Fetch failed.');
+      }
+      return res.json();
+    } catch (e) {
+      console.err('Error.');
+      return 'yo';
+    }
+
+  return (
+    <>
+      {rawPostData.content}
+    </>
+  );
+ 
