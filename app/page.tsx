092a0a697de5975e03411ab8840cbab29d475756
git <82054121+KXzeno@users.noreply.github.com>
2024-04-02 08:34:18 -0700

    refactor: switch filetype to typescriptreact

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-"use client";
 import React from 'react';
 import Image from 'next/image';
 import { Home } from './components';
 
 export default function Blog() {
-  const id = React.useId();
-  const userId = `${id}-username`;
-  const passId = `${id}-password`;
 
   return(
     <>
