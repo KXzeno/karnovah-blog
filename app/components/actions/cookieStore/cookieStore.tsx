35fc498e0f649cd317cf64ee02238a3640a4e2a9
git <82054121+KXzeno@users.noreply.github.com>
2024-04-06 12:28:16 -0700

    feat: create cookie setter server action

--- app/components/actions/cookieStore/cookieStore.tsx
+++ app/components/actions/cookieStore/cookieStore.tsx
@@ -2,6 +2,10 @@
 
 import { cookies } from "next/headers";
 
-async function handleCookie(data: any) {
+export async function handleCookie(data: any) {
+  let oneDay: number = 24 * 60 * 60 * 1000;
+  let death: number = oneDay * 71;
+
+  cookes().set('theme-cookie', data, { expires: death, maxAge: death });
 
 }
