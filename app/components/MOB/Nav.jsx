3f8e7a39fa186d6b4dc3922207fbcfaa6ef93630
git <82054121+KXzeno@users.noreply.github.com>
2024-04-09 09:28:30 -0700

    feat: add animation to nav backdrop

--- app/components/MOB/Nav.jsx
+++ app/components/MOB/Nav.jsx
@@ -18,6 +18,9 @@ export default function Nav() {
 
   let [showSubMenu, toggleSubMenu] = useToggle(false);
 
+  let id = React.useId(null);
+  let [hoveredNavItem, setHoveredNavItem] = React.useState(null);
+
   /**
    * Manual routers which otherwise toggles a sub menu
    * @param {object} e - event to access innerHTML or 'outerText'
@@ -39,31 +42,78 @@ export default function Nav() {
 
   let subMenuRef = React.useRef(null);
 
+     <motion.div
+      className="absolute inset-0 shadow-lg rounded-lg shadow-green-500/50"
+      layoutId={id}
+    />
+
+  let LINKS = [
+    {
+      slug: 'recent',
+      label: 'Recent',
+      href: '/recent'
+    },
+    {
+      slug: 'posts',
+      label: 'Posts',
+      href: '/posts'
+    },
+    {
+      slug: 'about',
+      label: 'About',
+      href: '/about'
+    }
+  ];
+
   return (
     <>
-      <nav id="nav-box">
-        <Link href="/recent" onClick={handleRoute} className="nav-btn">
-          Recent
-        </Link>
-        <Link 
-          href="/posts"
-          type="button" 
-          onClick={handleRoute} 
-          className="relative nav-btn-posts pointer-events-auto">
-          <p className="nav-btn inline">
-            Posts
-          </p>
-          <span 
-            onClick={handleDefault}
-            dir="rtl"
-            id="sub-menu"
-          >
-            ▼
-          </span>
-        </Link>
-        <Link href="/about" type="button" onClick={handleRoute} className="nav-btn">
-          About
-        </Link>
+      <nav id="nav-box" onMouseLeave={() => setHoveredNavItem(null)}>
+        <LayoutGroup>
+          { /* => { EXPRESSION = return manually } */ }
+          {LINKS.map(({ slug, label, href }) => ( 
+            <Link
+              key={slug}
+              href={href}
+              onMouseOver={() => setHoveredNavItem(slug)}
+              style={{
+                zIndex: hoveredNavItem === slug ? 1 : 2,
+              }}
+              className="nav-btn"
+            >
+              {label}
+              {hoveredNavItem === slug && (
+                <motion.div
+                  className="absolute inset-0 -mx-[0.31rem] shadow-lg rounded-xl shadow-green-500/50 shadow-[#A70AFF]"
+                  layoutId={id}
+                />
+              )}
+            </Link>
+          ))}
+          {/*
+          <Link href="/recent" onClick={handleRoute} className="nav-btn">
+            Recent
+          </Link>
+          <Link 
+            href="/posts"
+            type="button" 
+            onClick={handleRoute} 
+            className="relative nav-btn-posts pointer-events-auto">
+            <p className="nav-btn inline">
+              Posts
+            </p>
+            <span 
+              onClick={handleDefault}
+              dir="rtl"
+              id="sub-menu"
+            >
+              ▼
+            </span>
+          </Link>
+          <Link href="/about" type="button" onClick={handleRoute} className="nav-btn">
+            About
+          </Link>
+          */}
+        </LayoutGroup>
         <div className={`${dollarSigns != 0 ? 'visible' : 'hidden'} absolute right-8 h-16 w-16`}>
           <LayoutGroup>
             <motion.button 
