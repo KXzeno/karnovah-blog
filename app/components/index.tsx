e37e2583c35e2d120d8b3443517fb59f4ba9e769
git <82054121+KXzeno@users.noreply.github.com>
2024-04-09 07:28:06 -0700

    refactor: encapsulate exports in route

---
+++ app/components/index.tsx
@@ -0,0 +1,17 @@
+import Home from './Home';
+import Nav from '@M/Nav';
+import SubMenu from '@F/SubMenu';
+import Footer from '@M/Footer';
+import Icons, { Warning } from '@M/Icons';
+import TableOfContents from '@M/ToC';
+import useDetectResize from '@H/useDetectResize';
+
+export {
+  Home,
+  Nav,
+  SubMenu,
+  Footer,
+  Icons, Warning,
+  TableOfContents,
+  useDetectResize,
+}
