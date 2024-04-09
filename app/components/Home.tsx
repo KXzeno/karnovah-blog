bc1718dab575455f79c442543e2461d0836362b4
git <82054121+KXzeno@users.noreply.github.com>
2024-04-09 07:25:03 -0700

    refactor: type annotate export

---
+++ app/components/Home.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Nav from '@M/Nav';
+import Footer from '@M/Footer';
+import Feed from '@F/Feed';
+
+interface HomeProps {
+  children: React.ReactNode;
+}
+
+export default function Home({
+  children,
+}: HomeProps): React.ReactElement {
+
+  return (
+    <React.Fragment>
+      <Nav />
+      <Feed />
+      <Footer />
+    </React.Fragment>
+  );
+}
