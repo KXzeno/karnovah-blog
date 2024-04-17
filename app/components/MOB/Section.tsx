ee1c70b8fd1b7635c8f5b93d10b37e839977b031
git <82054121+KXzeno@users.noreply.github.com>
2024-04-16 20:53:45 -0700

    REFACTOR: transform object and methods to tsx

--- app/components/MOB/Section.tsx
+++ app/components/MOB/Section.tsx
@@ -75,214 +75,245 @@ export default React.memo(function Section({
     //let start = performance.now()
     // Can pass in prototype as an object to defineProps
     // E.g., String.prototype
-    let NodesList = {}
-
-    interface ClickEvent {
-        target: {
-            parentElement: HTMLElement,
-        },
+    type htmlLC = {
+      collection?: HTMLCollection,
+      getList?: HTMLCollection,
+      setListener?: boolean,
+      retrieve?: Function,
+      smartObserve?: Function,
     }
+
+    let elemCollection: htmlLC = {};
 
     /**
      * Click handler for toc elements to properly display new highlight
-     * @param {object} target - Destructured element from event object
+     * @param {object} event - Destructured element from event object
      * @author Kx
      */
-    let handleClick = ({ target: { parentElement: element } }: ClickEvent) => {
-      let index = element.getAttribute('data-index');
+    let handleClick = (event: Event) => {
+      let { target } = event;
+      let element = (target as HTMLElement).parentElement;
+      if (!element) return;
+      let index: string | null = element.getAttribute('data-index');
       function toggleOnClick() {
-        NodesList.getList.forEach((e) => {
-          e === NodesList.getList[index] 
-            ? e.setAttribute('class', 'curr-head')
-            : e.removeAttribute('class', 'curr-head');
-        });
-      }
-      setTimeout(toggleOnClick, 70);
-      setIsListening(() => true);
-      NodesList.setListener = true;
-    }
+        if (elemCollection && elemCollection.getList) {
+          let list: HTMLCollection = elemCollection.getList;
 
-    if (!NodesList.collection) {
-      Object.defineProperties(NodesList, {
-        // HTML Collection of ToC items
-        collection: {
-          value: document.querySelectorAll('[name$="-*"]')
-        },
-        /**
-         * Getter
-         * @function
-         * @returns {NodeList | object} Returns collection prop
-         * @author Kx
-         */
-        getList: {
-          get: function() {
-            return this.collection;
-          },
-          enumerable: true,
-          configurable: false,
-        },
-        /**
-         * Mutates collection
-         * @deprecated - HTML Collections / NodeLists allow for inline mutation
-         * @param {NodeList | object} nodes - A collection of nodes to mutate object
-         * @author Kx
-         */
-        setList: {
-          set: function(nodes) {
-            this.collection = collection;
+          for (let i = 0; i < list.length; i++) {
+            let parsedElem: Element | null;
+            let e: Element | null = list.item(i);
+            if (index && e) {
+              parsedElem = list.item(Number(index));
+              e === parsedElem
+                ? e.classList.add('curr-head')
+                : e.classList.remove('curr-head');
+            }
           }
-        },
-        /**
-         * Retrieves toc element linked with section ref
-         * @function
-         * @returns {Node | object} Toc node or null
-         * @author Kx
-         */
-        retrieve: {
-          value: function() {
-            let returnedNode;
-            this.collection.forEach((node) => {
-              if (node.getAttribute('name') === `${secRef.current.id}-*`) {
-                return returnedNode = node;
+          setTimeout(toggleOnClick, 70);
+          setIsListening(() => true);
+          elemCollection.setListener = true;
+        }
+      } 
+      if (!elemCollection.collection) {
+        Object.defineProperties(elemCollection, {
+          // HTML Collection of ToC items
+          collection: {
+            value: function(): HTMLCollection | null {
+              let parentElem = document.getElementById('toc-list');
+              if (parentElem && parentElem.children) {
+                return parentElem.children;
               }
-            });
-            return returnedNode || null;
+              return null;
+            }
           },
-          writable: false,
-          enumerable: false,
-          configurable: false,
-        },
-        /**
-         * Handles dynamic class toggling across multiple observed sections
-         * @function
-         * @param {Node object} target - retrieved node used for collation
-         * @author Kx
-         */
-        smartObserve: {
-          value: function(target) {
-            Object.defineProperty(NodesList, 'activeStack', {
-              value: [],
-              writable: true,
-              enumerable: true,
-            });
-
-            // Executes only when observing element id exists
-            if (/*onScreen && */secRef.current.id) {
-
-              // Toggles highlight on all toc items
-              this.collection.forEach((node) => {
-                node === target && node.setAttribute('class', 'curr-head');
-                // Toggle attribute on observer dismissal, ensures end node removal
-                !onScreen && Number(target.getAttribute('data-index')) !== 0
-                  && target.getAttribute('name') && target.toggleAttribute('class');
-                node.getAttribute('class') && this.activeStack.push(node); 
+          /**
+           * Getter
+           * @function
+           * @returns {NodeList | object} Returns collection prop
+           * @author Kx
+           */
+          getList: {
+            get: function(): HTMLCollection {
+              return this.collection;
+            },
+            enumerable: true,
+            configurable: false,
+          },
+          /**
+           * Mutates collection
+           * @deprecated - HTML Collections / NodeLists allow for inline mutation
+           * @param {NodeList | object} nodes - A collection of nodes to mutate object
+           * @author Kx
+           */
+          setList: {
+            set: function(collection) {
+              this.collection = collection;
+            }
+          },
+          /**
+           * Retrieves toc element linked with section ref
+           * @function
+           * @returns {Node | object} Toc node or null
+           * @author Kx
+           */
+          retrieve: {
+            value: function() {
+              let returnedElement: Element | null = null;
+              this.collection.forEach((e: Element) => {
+                if (e.getAttribute('name') === `${secRef.current.id}-*`) {
+                  return returnedElement = e;
+                }
+              });
+              return returnedElement;
+            },
+            writable: false,
+            enumerable: false,
+            configurable: false,
+          },
+          /**
+           * Handles dynamic class toggling across multiple observed sections
+           * @function
+           * @param {Node object} target - retrieved node used for collation
+           * @author Kx
+           */
+          smartObserve: {
+            value: function(target: Element) {
+              Object.defineProperty(elemCollection, 'activeStack', {
+                value: [],
+                writable: true,
+                enumerable: true,
               });
 
-              // Detoggles previous node's classes
-              for (let val of this.collection.values()) {
-                if (this.activeStack.length > 1) {
-                  this.activeStack.length != 1 
+              // Executes only when observing element id exists
+              if (/*onScreen && */secRef.current.id) {
+
+                // Toggles highlight on all toc items
+                this.collection.forEach((e: Element) => {
+                  e === target && e.setAttribute('class', 'curr-head');
+                  // Toggle attribute on observer dismissal, ensures end e removal
+                  !onScreen && Number(target.getAttribute('data-index')) !== 0
+                  && target.getAttribute('name') && target.toggleAttribute('class');
+                  e.getAttribute('class') && this.activeStack.push(e); 
+                });
+
+                // Detoggles previous node's classes
+                for (let val of this.collection.values()) {
+                  if (this.activeStack.length > 1) {
+                    this.activeStack.length != 1 
                     && val.getAttribute('class')
                     && this.activeStack.pop() 
                     && val.removeAttribute('class', 'curr-head');
-                }
+                  }
 
-                // Highlights previous node when escaping last node
-                let index = this.activeStack.length === 0
+                  // Highlights previous node when escaping last node
+                  let targetElem = elemCollection.getList;
+                  let index = this.activeStack.length === 0
                   && `${secRef.current.id}-*` === val.getAttribute('name')
                   && val.getAttribute('data-index');
-                index && NodesList.getList[index - 1]?.setAttribute('class', 'curr-head');
+                  index && targetElem && targetElem[index - 1]?.setAttribute('class', 'curr-head');
+                }
               }
+            },
+            enumerable: true,
+          },
+          setListener: {
+            set: function(boolean) {
+              this.listener = boolean;
             }
           },
-          enumerable: true,
-        },
-        setListener: {
-          set: function(boolean) {
-            this.listener = boolean;
-          }
-        },
-        getListener: {
-          get: function(boolean) {
-            return this.listener;
-          }
-        },
-      });
-      NodesList.getList.forEach((node) => {
-        !isListening && node.firstElementChild.addEventListener('click', handleClick, { once: true }); 
-      });
-    }
-
-    let [nodes, targetNode] = [NodesList.getList, NodesList.retrieve()];
-    NodesList.smartObserve(targetNode);
-    //let end = performance.now();
-    //console.log(`${end - start}`);
-    return () => {
-      if (isListening) {
-        nodes.forEach((node) => {
-          node.removeEventListener('click', handleClick);
+          getListener: {
+            get: function() {
+              return this.listener;
+            }
+          },
         });
-        //console.log('Event dismissed');
-        setIsListening(() => false);
+        let list = elemCollection.getList;
+        list && (() => {
+          for (let i = 0; i < list.length; i++) {
+            let parsedElem: Element | null = list.item(i);
+            if (!isListening && parsedElem?.firstElementChild) {
+              parsedElem.addEventListener('click', handleClick, { once: true }); 
+            }
+          }})();
       }
-      //console.log('Render discarded.');
-    }
-  }, [isListening, onScreen, isMobileLandscape]);
+      let [elements, targetElem]: [HTMLCollection, Element | undefined];
+      if (elemCollection.retrieve !== undefined) {
+        targetElem = elemCollection.retrieve();
+      }
 
-  React.useEffect(() => {
-    let getNodes = new Promise((resolve, reject) => {
-      let intervalId = setInterval(() => {
-        let nodes = document.querySelectorAll('[data-index]');
-        if (nodes.length > 0) {
-          clearInterval(intervalId);
-          resolve(nodes);
+      if (elemCollection && elemCollection.getList && elemCollection.smartObserve) {
+        elements = elemCollection.getList;
+        elemCollection.smartObserve(targetElem); 
+      }
+
+      //let end = performance.now();
+      //console.log(`${end - start}`);
+      return () => {
+        if (isListening) {
+          elements.forEach((element) => {
+            element.removeEventListener('click', handleClick);
+          });
+          //console.log('Event dismissed');
+          setIsListening(() => false);
         }
-      }, 100);
-    });
-
-    getNodes.then((nodes) => {
-      document.querySelectorAll('[data-index]').forEach((child, i) => {
-        i === 0 && child.setAttribute('class', 'curr-head');
-      });
-    }).catch((e) => {
-      console.error(e);
-    }).finally(() => {
-    });
-  }, []);
-
-  React.useEffect(() => {
-    let getElements = new Promise((resolve, reject) => {
-      let elements = document.querySelectorAll('h3, h4');
-      if (elements.length > 0) {
-        resolve(elements) 
+        //console.log('Render discarded.');
       }
-    });
+    }, [isListening, onScreen, isMobileLandscape]);
 
-    getElements.then((elements) => {
-      elements.forEach((element, i, list) => {
-        if (elementStack.length !== list.length) {
-          setElementStack(prev => [...prev, element]);
-          //console.log(list.length, elementStack.length);
+    React.useEffect(() => {
+      let getNodes = new Promise((resolve, reject) => {
+        let intervalId = setInterval(() => {
+          let nodes = document.querySelectorAll('[data-index]');
+          if (nodes.length > 0) {
+            clearInterval(intervalId);
+            resolve(nodes);
+          }
+        }, 100);
+      });
+
+      getNodes.then((nodes) => {
+        document.querySelectorAll('[data-index]').forEach((child, i) => {
+          i === 0 && child.setAttribute('class', 'curr-head');
+        });
+      }).catch((e) => {
+        console.error(e);
+      }).finally(() => {
+      });
+    }, []);
+
+    React.useEffect(() => {
+      let getElements = new Promise((resolve, reject) => {
+        let elements = document.querySelectorAll('h3, h4');
+        if (elements.length > 0) {
+          resolve(elements) 
         }
       });
-    });
 
-    return () => {
-      let elementsLength = document.querySelectorAll('h3, h4').length;
-      if (elementStack.length > elementsLength) {
-        setElementStack(prev => prev.splice(elementsLength, prev.length - 1));
-      }
-    };
-  }, []);
+      getElements.then((elements) => {
+        elements.forEach((element, i, list) => {
+          if (elementStack.length !== list.length) {
+            setElementStack(prev => [...prev, element]);
+            //console.log(list.length, elementStack.length);
+          }
+        });
+      });
 
-  React.useEffect(() => {
-    let nodes = document.querySelectorAll('[data-index]');
-    isMobileLandscape && 
-      +(() => {
+      return () => {
+        let elementsLength = document.querySelectorAll('h3, h4').length;
+        if (elementStack.length > elementsLength) {
+          setElementStack(prev => prev.splice(elementsLength, prev.length - 1));
+        }
+      };
+    }, []);
+
+    React.useEffect(() => {
+      let nodes = document.querySelectorAll('[data-index]');
+      isMobileLandscape && 
+        +(() => {
         nodes.forEach((node, i, list) => {
           (node.getAttribute('class') === 'curr-head') 
-            && (i === list.length - 1) 
-            && list[0].setAttribute('class', 'curr-head');
+          && (i === list.length - 1) 
+          && list[0].setAttribute('class', 'curr-head');
         });
       })();
 
@@ -299,31 +330,31 @@ export default React.memo(function Section({
       }
 
       standaloneFunction();
-/**
- * This is a description of the function.
- *
- * @param {number} x - The first number to add.
- * @param {number} y - The second number to add.
- * @returns {number} The sum of `x` and `y`.
- *
- * @example
- * // returns 3
- * add(1, 2);
- */
-function add(x, y) {
-  return x + y;
-}
+      /**
+       * This is a description of the function.
+       *
+       * @param {number} x - The first number to add.
+       * @param {number} y - The second number to add.
+       * @returns {number} The sum of `x` and `y`.
+       *
+       * @example
+       * // returns 3
+       * add(1, 2);
+       */
+      function add(x, y) {
+        return x + y;
+      }
 
 
-  }, [isMobileLandscape]);
+    }, [isMobileLandscape]);
 
-  return (
-    <Section
-      id={idInserter()}
-      ref={secRef}
-      {...delegated}
-    >
-      {children}
-    </Section>
-  )
-});
+    return (
+      <Section
+        id={idInserter()}
+        ref={secRef}
+        {...delegated}
+      >
+        {children}
+      </Section>
+    )
+  });
