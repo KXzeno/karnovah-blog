c90c6e551033f5039b715be587ee23ab7286299a
git <82054121+KXzeno@users.noreply.github.com>
2024-04-18 10:16:37 -0700

    Refactor btn polymorph component to TS

--- app/components/MOB/LinkButton.tsx
+++ app/components/MOB/LinkButton.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+interface FCProps {
+  href: string,
+  children: React.ReactNode,
+  className: string | undefined,
+}
+
 export default function LinkButton({ 
   href, 
   children, 
   className,
   ...delegated 
-}) {
+}: FCProps) {
   const Tag = typeof href === 'string'
     ? 'a'
     : 'button';
