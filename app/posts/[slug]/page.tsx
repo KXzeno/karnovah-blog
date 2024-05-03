ad97c34c2621c4ca1c69d2a2a0b5384f35255151
git <82054121+KXzeno@users.noreply.github.com>
2024-05-03 00:08:25 -0700

    refactor: strong-type param and returnType of RFC

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,21 +1,23 @@
-export async function generateMetadata({ params, searchParams }, parent) {
-  // read route params
-  const { slug } = params;
+export async function generateMetadata(
+  { params, searchParams }: { params: { slug: string }, searchParams: object }, 
+  parent: object) {
+    // read route params
+    const { slug } = params;
 
-  // fetch data
-  const post = await fetch(`https://blog.karnovah.com/posts/${slug}`).then((res) => res.json());
+    // fetch data
+    const post = await fetch(`https://blog.karnovah.com/posts/${slug}`).then((res) => res.json());
 
-  // optionally access and extend (rather than replace) parent metadata
-  // const previousImages = (await parent).openGraph?.images || []
+      // optionally access and extend (rather than replace) parent metadata
+      // const previousImages = (await parent).openGraph?.images || []
 
-  return {
-    title: `${post.title}`,
-    /*
-    openGraph: {
-      images: ['/path/to/img', ...previousImages],
-    },
-    */
+      return {
+      title: `${post.title}`,
+      /*
+         openGraph: {
+         images: ['/path/to/img', ...previousImages],
+         },
+       */
+    }
   }
-}
 
-export default function Page({ params, searchParams }) {}
+  export default function Page({ params, searchParams }) {}
