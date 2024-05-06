7ed40b5ceafd18cb47a5ebcb3ff14095ee35bf06
git <82054121+KXzeno@users.noreply.github.com>
2024-05-06 02:19:21 -0700

    Add Author RFC boilerplate

---
+++ app/components/FOB/Authors/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Author() {
+  return (
+    <div id="author-profile">
+      <div id="author-name">
+        <div id="author-level">
+        </div>
+      </div>
+    </div>
+  );
+}
