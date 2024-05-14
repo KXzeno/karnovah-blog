99c0224216f8fe898b9a2624ef0c5121276b4fee
git <82054121+KXzeno@users.noreply.github.com>
2024-05-14 05:07:02 -0700

    Store author json in reference

--- app/components/FOB/Authors/Author.tsx
+++ app/components/FOB/Authors/Author.tsx
@@ -13,6 +13,8 @@ async function getFile(filePath: string) {
 }
 
 export default function Author({ user }: { user: string }): React.ReactNode {
+  let fileData = getFile('components/FOB/Authors/Author');
+
   return (
     <div id="author-profile">
       <div id="author-name">
