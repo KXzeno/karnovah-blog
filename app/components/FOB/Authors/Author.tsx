5b464795b06655aba4bf29fe192286ac6eb3b77c
git <82054121+KXzeno@users.noreply.github.com>
2024-05-23 00:11:56 -0700

    style: adjust box layout of author RFC

--- app/components/FOB/Authors/Author.tsx
+++ app/components/FOB/Authors/Author.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { readFile } from 'fs/promises';
 import Image from 'next/image';
 
-  async function getFile(filePath: string) {
-    try {
-      let data = await readFile(`./app/${filePath}`, { encoding: 'utf8' });
-      let content = JSON.parse(data)
-      return content;
-    } catch (err) {
-      console.error(err);
-    }
+async function getFile(filePath: string) {
+  try {
+    let data = await readFile(`./app/${filePath}`, { encoding: 'utf8' });
+    let content = JSON.parse(data)
+    return content;
+  } catch (err) {
+    console.error(err);
   }
+}
 
-  export default async function Author({ user }: { user: string }): Promise<React.AwaitedReactNode> {
+export default async function Author({ user }: { user: string }): Promise<React.AwaitedReactNode> {
   let authors = await getFile('components/FOB/Authors/authorList.json');
   if (!user || !authors) {
     throw new Error('Invalid user');
@@ -20,10 +20,10 @@ import Image from 'next/image';
   console.log(authors[user]);
 
   return (
-    <div id="author-profile">
-      <Image className='float-right' src={`/${user}.png`} alt='author-img' width='32' height='32'/>
-      <div id="author-name" className='relative pt-[8px] text-right'>
-        <span className="pr-2">{`${user}`}</span>
+    <div id="author-profile" className="flex w-full place-content-end">
+      <div id="author-name" className='flex relative w-max pt-[8px] text-right'>
+        <span className="pr-3 place-self-end">{`${user}`}</span>
+        <Image className='float-right' src={`/${user}.png`} alt='author-img' width='32' height='32'/>
         <div id="author-level">
         </div>
       </div>
