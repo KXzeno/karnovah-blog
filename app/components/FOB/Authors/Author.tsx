0e91d7f69809c3656562355bebfd1c55f738855a
git <82054121+KXzeno@users.noreply.github.com>
2024-05-26 12:04:20 -0700

    style: change display and position for date & user

--- app/components/FOB/Authors/Author.tsx
+++ app/components/FOB/Authors/Author.tsx
@@ -20,11 +20,11 @@ export default async function Author({ user, date }: { user: string, date: strin
   console.log(authors[user]);
 
   return (
-    <div id="author-profile" className="flex w-full place-content-end">
-      <div id="author-name" className='flex relative w-max pt-[8px] text-right'>
-        <span className="pr-3 self-end">{`${date}`}</span>
-        <span className="pr-3 place-self-end">{`${user}`}</span>
-        <Image className='float-right' src={`/${user}.png`} alt='author-img' width='32' height='32'/>
+    <div id="author-profile" className="flex relative w-full">
+      <span className="pr-3 self-end">{`${date}`}</span>
+      <div id="author-name" className='absolute inline-flex right-0 bottom-0 place-self-end w-max pt-[8px]'>
+          <span className="inline-block pr-3 align-text-bottom self-end">{`${user}`}</span>
+          <Image className='float-right' src={`/${user}.png`} alt='author-img' width='32' height='32'/>
         <div id="author-level">
         </div>
       </div>
