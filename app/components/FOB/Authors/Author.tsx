31d06c40435a64c88d62cd88512c7e80858b3903
git <82054121+KXzeno@users.noreply.github.com>
2024-05-27 10:45:52 -0700

    refactor: encapsulate styles in id selectors

--- app/components/FOB/Authors/Author.tsx
+++ app/components/FOB/Authors/Author.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { readFile } from 'fs/promises';
 import Image from 'next/image';
+import './Authors.css';
 
 async function getFile(filePath: string) {
   try {
@@ -20,13 +21,11 @@ export default async function Author({ user, date }: { user: string, date: strin
   console.log(authors[user]);
 
   return (
-    <div id="author-profile" className="flex relative w-full">
-      <span className="pr-3 self-end">{`${date}`}</span>
-      <div id="author-name" className='absolute inline-flex right-0 bottom-0 place-self-end w-max pt-[8px]'>
-          <span className="inline-block pr-3 align-text-bottom self-end">{`${user}`}</span>
-          <Image className='float-right' src={`/${user}.png`} alt='author-img' width='32' height='32'/>
-        <div id="author-level">
-        </div>
+    <div id="author-profile">
+      <span id="publish-date">{`${date}`}</span>
+      <div id="author-name">
+          <span>{`${user}`}</span>
+          <Image src={`/${user}.png`} alt='author-img' width='32' height='32'/>
       </div>
     </div>
   );
