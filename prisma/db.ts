ebc27ce41f142aa6deedf863e541433b836824d4
git <82054121+KXzeno@users.noreply.github.com>
2024-07-23 14:00:00 -0700

    fix(dependency): add Prisma instantiator

---
+++ prisma/db.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import 'dotenv/config';
+
+/**
+* @link https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
+*/
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL_NEON } } });
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
