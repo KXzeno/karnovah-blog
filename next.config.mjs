98380a7351a85450bec49e940ac75aee1f5b9fc1
git <82054121+KXzeno@users.noreply.github.com>
2024-07-24 20:39:53 -0700

    Add BMC remote pattern
    Remote patterns exist as Nextjs's safety measures for handling external
    scripts

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.buymeacoffee.com',
+        port: '',
+      }
+    ]
+  }
   // reactStrictMode: false,
 };
 
