9a5cb0f616a105cabbd52559a463e9ebb768f106
git <82054121+KXzeno@users.noreply.github.com>
2024-07-24 20:46:49 -0700

    Append BMC RFC

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
 import { Home } from './components';
+import dynamic from 'next/dynamic';
+let BuyMeACoffeeWidget = dynamic(() => import('@F/BuyMeACoffee')); 
 
 export default function Blog() {
 
   return(
     <>
       <Home />
+      <BuyMeACoffeeWidget />
     </>
   );
 }
