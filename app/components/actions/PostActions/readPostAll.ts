5f726a7d4da9c674f001990ec371f11a8f4e3ac0
git <82054121+KXzeno@users.noreply.github.com>
2024-07-25 08:05:02 -0700

    feat: implement parameter-based sort on query

--- app/components/actions/PostActions/readPostAll.ts
+++ app/components/actions/PostActions/readPostAll.ts
@@ -2,15 +2,35 @@
 
 import { prisma } from "@/prisma";
 
-export async function readPostAll(placeholder?: unknown) {
-  try {
-    let db = await prisma.post.findMany({ 
-      include: {
-        Section: true,
-      },
-    });
-    return db;
-  } catch (error) {
-    console.error(error);
-  }
+interface SortParams {
+  field: string,
+  value: string,
+}
+
+export async function readPostAll(params?: SortParams) {
+  if (params)
+    try {
+      let db = await prisma.post.findMany({ 
+        orderBy: {
+          [params.field]: params.value
+        },
+        include: {
+          Section: true,
+        },
+      });
+      return db;
+    } catch (error) {
+      console.error(error);
+    } else {
+      try {
+        let db = await prisma.post.findMany({ 
+          include: {
+            Section: true,
+          },
+        });
+        return db;
+      } catch (error) {
+        console.error(error);
+      }
+    }
 }
