2069ddcae56ddc25e90528cda0da3fa6b939bda2
git <82054121+KXzeno@users.noreply.github.com>
2024-07-27 19:45:38 -0700

    "PERF!: reduce overhead 1.7613ms -> 0.1217ms"
    Replace recursion logic with iteration, having immensely reduced memory complexity and one function call which deters nested stack traces in runtime

--- app/components/MOB/Post.tsx
+++ app/components/MOB/Post.tsx
@@ -24,103 +24,23 @@ interface Section {
   aside: string[];
 }
 
-// Declare data structure to hold intermediate elements
-let jsxLinkedList = new SinglyLinkedList<React.ReactNode>();
-
-/**
- * Iterate over JSX List and output elements recursively
- * @param {SinglyLinkedList} list - Data structure to iterate
- * @returns {ReactNode} A react node to dynamically render in RFC
- * @author Kx
- */
-function renderJSX(list: SinglyLinkedList<React.ReactNode>, opt?: { renderContent: boolean }): React.ReactNode {
-  if (list.getSize() === 0) return;
-  let nodeArr: React.ReactNode[] = [];
-  if (opt && opt.renderContent === true) {
-    let contentNode: React.ReactNode[] = [];
-
-    // Separately handle recursion over section content
-    while (list.getHead() !== null) {
-      // Use type assertion to access react-dom props (node.type)
-      let node: React.ReactNode = list.removeFirst() as React.ReactElement;
-      let newHead: React.ReactNode = list.first() as React.ReactElement;
-      if (newHead && typeof newHead.type === 'object' || list.getTail() === null) { 
-        // Continues section head recursion
-        return contentNode;
-      }
-      contentNode.push(node);
-    }
-    return;
+function project(sections: Section[]): React.ReactNode {
+  let nodeG: React.ReactNode[] = [];
+  for (let i = 0; i < sections.length; i++) {
+    let hdr = sections[i].header ?? sections[i].subheader;
+    (hdr === sections[i].header) ?
+      nodeG.push(<Section>{hdr}</Section>) :
+      nodeG.push(<Section as='subsec'>{hdr}</Section>);
+    let contents = sections[i].content.flatMap((par) => (<p>{par}</p>));
+    nodeG.push(<section>{[...contents]}</section>);
   }
-
-  // Separately handle recursion over section headers
-  while (list.getHead() !== null) {
-    let node: React.ReactNode = list.removeFirst() as React.ReactElement;
-    // Dynamically wrap section tag around section children
-    let isHeader = typeof node.type === 'object';
-
-    if (!isHeader) {
-      // Calls section content recursion
-      node = <section>{renderJSX(list, { renderContent: true })}</section>;
-    }
-
-    nodeArr.push(node);
-  }
-  console.log(...nodeArr);
-  return [...nodeArr];
-
-  /** @deprecated
-   *  Ternary recursion technique creates nested section
-   *  elements, allowing zero mutation to control flow
-   *  
-   * return (!isHeader && isPrevHead) ? 
-   *   (<section>{node}{renderJSX(list, isHeader)}</section>) :
-   *   (<>{node}{renderJSX(list, isHeader)}</>); */
-}
-
-/** {@inheritDoc renderJSX}
- * Iterate over queried db data recursively and compiles
- * to React elements using globally defined linked list
- * @param {Section[]} data - Individual content queried from database
- * @param {number} step - Trace identifier for indexing data
- * @returns {ReactNode} An element with embedded function calls to render other elements
- * @author Kx
- */
-async function recurseData(data: Array<Section>, step?: number): Promise<React.AwaitedReactNode> {
-  if (step === undefined) step = 0;
-  if (step === data.length) {
-    return (
-      <>
-        {renderJSX(jsxLinkedList)}
-      </>
-    );
-  } else { step += 1; }
-  let section = data[step - 1];
-  if (section.header === null && section.subheader === null) {
-    throw new Error('Data has no headers.');
-  }
-  // Nullish coalescence logic doesn't satisfy TS control flow?
-  let [header, isSubHeader]: [string, boolean] = [(section.header ?? section.subheader) as string, !!section.subheader];
-  let content = section.content;
-
-  let headerNode = (isSubHeader === false) ?
-    <Section>{header}</Section> : <Section as='subsec'>{header}</Section>;
-
-  jsxLinkedList.addLast(headerNode);
-
-  for (let i = 0; i < content.length; i++) {
-    let contentNode = (<p>{content[i]}</p>);
-    jsxLinkedList.addLast(contentNode);
-  }
-
-  return recurseData(data, step);
+  return [...nodeG];
 }
 
 export default async function Post({ param }: { param: string }): Promise<React.AwaitedReactNode> {
   let post = await readPost(param);
   if (post === null || post === undefined) notFound();
   // LOCAL: @ts-expect-error
-  console.log(post);
   let sections: Array<Section> = post.sections;
 
   return (
@@ -133,7 +53,7 @@ export default async function Post({ param }: { param: string }): Promise<React.
         {post.description}
       </SubHeader>
       <PrimaryContent>
-        {recurseData(sections)}
+        {project(sections)}
       </PrimaryContent>
     </ArticleProvider>
   );
