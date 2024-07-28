c3161049c277014a4e13dfb8fcb73502fe3573c5
git <82054121+KXzeno@users.noreply.github.com>
2024-07-28 15:54:07 -0700

    fix: use compatible methods

--- utils/RectifySchema.java
+++ utils/RectifySchema.java
@@ -2,7 +2,8 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.IOException;
-import java.nio.file.StandardOpenOption;
+// import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 public class RectifySchema {
   // File schemaFile = new File("prisma/schema.prisma");
@@ -11,12 +12,18 @@ public class RectifySchema {
   public void rectify() {
     File schemaFile = new File("prisma/schema.prisma");
     Path schemaPath = schemaFile.toPath();
+    StringBuilder finalContent = new StringBuilder();
 
     try {
-      String content = Files.readString(schemaPath);
-      content = content.replaceAll("\\bSection\\b(?!\\s*([\\{\\[]))\\b", "sections");
-      content = content.replaceAll("\\bCategory\\b(?!\\s*([\\{\\[]))\\b", "categories");
-      Files.write(schemaPath, content.getBytes());
+      List<String> contents = Files.readAllLines(schemaPath);
+      while (contents.isEmpty() != true) {
+        String content = contents.get(0);
+        content = content.replaceAll("\\bSection\\b(?!\\s*([\\{\\[]))\\b", "sections");
+        content = content.replaceAll("\\bCategory\\b(?!\\s*([\\{\\[]))\\b", "categories");
+        finalContent.append(content + "\n");
+        contents.remove(0);
+      }
+      Files.write(schemaPath, finalContent.toString().getBytes());
     } catch (IOException x) {
       x.printStackTrace();
     }
