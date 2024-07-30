f3eb8c0cea832f7ba2facbe4311e84beaad1a19b
git <82054121+KXzeno@users.noreply.github.com>
2024-07-30 16:31:25 -0700

    FEAT: separate initial fetch from Feed RFC
    The main concern was cumulative layout shifting on each refresh/visit to
    the page, however the issue was that I was using asynchronous functions
    combined with hooks to dynamically fetch data, including initial fetch.
    Using a server component to pre-render is ideal.

---
+++ app/components/MOB/Landing.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { readPostAll, getInitialId } from '@A/PostActions';
+import Feed from '@F/Feed';
+
+interface Post {
+  post_id: number;
+  title: string;
+  createdAt: Date;
+  published: boolean;
+  subtitle: string;
+  description: string;
+  choice: number | null;
+}
+
+// FOR INITIAL DATA, STOP GETTING CONFUSED FROM COMBINING PAGINATION.
+// DO PAGINATION ASYNCHRONOUSLY IN CLIENT COMPONENT
+
+export default async function Landing() {
+  let initialData: Post[] | undefined = await readPostAll({ field: 'createdAt', value: 'desc' });
+
+  if (!initialData) {
+    throw new Error('Query for posts failed.');
+  }
+
+  let initialCursor = initialData[initialData.length - 1].post_id;
+
+  return (
+    <>
+      <Feed initialData={initialData} initialCursor={initialCursor}/>
+    </>
+  )
+}
