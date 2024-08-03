90d8eae19c015bd386b90ba8d76c6ffb203089f4
git <82054121+KXzeno@users.noreply.github.com>
2024-08-02 18:40:23 -0700

    fix: move early return to component mount

--- app/components/hooks/useIntersect.tsx
+++ app/components/hooks/useIntersect.tsx
@@ -2,26 +2,19 @@
 import React from 'react';
 
 export default function useIntersect(elementA: HTMLElement | null, elementB: HTMLElement | null): boolean | void {
-  if (!(elementA !== null && elementB !== null)) {
-    return;
-  }
-
   // Vertical downward collapse,
   // TODO: Handle all intersections
   let [intersected, setIntersected] = React.useState<boolean>(false);
 
-  let rectA = elementA.getBoundingClientRect();
-  let rectB = elementB.getBoundingClientRect();
-
   React.useEffect(() => {
-    if (rectB.bottom < rectA.bottom) {
-      setIntersected(true);
-    } else if (rectB.bottom > rectA.bottom) {
-      setIntersected(false);
+    if (!(elementA !== null && elementB !== null)) {
+      return;
     }
-  });
+    let rectA = elementA.getBoundingClientRect();
+    let rectB = elementB.getBoundingClientRect();
 
-  let memoizedIntersected = React.useMemo(() => intersected, [intersected]);
+    setIntersected(rectB.bottom < rectA.bottom);
+  }, [elementA, elementB]);
 
-  return memoizedIntersected;
+  return intersected;
 }
