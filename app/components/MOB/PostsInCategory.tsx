12fe5892be1922888cd5e4487e317b71fd905e67
git <82054121+KXzeno@users.noreply.github.com>
2024-08-02 18:22:00 -0700

    Init base RFC for nested category routes

---
+++ app/components/MOB/PostsInCategory.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function PostsInCategory({ category }: { category: unknown }) {
+  // @ts-expect-error
+  let posts = category.posts;
+
+  // TODO: dash-case url and titles when handling server side
+
+  return (
+    <main className='cat-wrapper'>
+      <section>
+        <ul>
+        {/* @ts-expect-error */
+          posts.reverse().map(post => {
+            let title = post.title;
+            let date = post.createdAt.toISOString().split('T')[0];
+            return (
+              <li>
+                <time>{`${date}`}</time>
+                <span>&#11116;</span>
+                <Link href={`/${title.toLowerCase()}`}>{`${title}`}</Link>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </main>
+  )
+}
