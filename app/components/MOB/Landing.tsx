72d5cde0bf5daba904168af7fc9396f53cbf02c3
git <82054121+KXzeno@users.noreply.github.com>
2024-10-09 15:13:01 -0700

    fix: revalidate path on redirect

--- app/components/MOB/Landing.tsx
+++ app/components/MOB/Landing.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { readPostAll, getInitialId } from '@A/PostActions';
 import Feed from '@F/Feed';
+import { revalidatePath } from 'next/cache';
 
 interface Post {
   post_id: number;
@@ -15,9 +16,13 @@ interface Post {
 // FOR INITIAL DATA, STOP GETTING CONFUSED FROM COMBINING PAGINATION.
 // DO PAGINATION ASYNCHRONOUSLY IN CLIENT COMPONENT
 
+// FIXME: Add auto update on altered / added data
+// Seems to only update if the site is republished
+// Robust fix: compare updated date and creation date
+
 export default async function Landing() {
   let initialData: Post[] | undefined = await readPostAll({ orderBy: { createdAt: 'desc' } });
-
+  revalidatePath('/');
   if (!initialData) {
     throw new Error('Query for posts failed.');
   }
