7cb0f5d63458bcd17ba64acfdc667bc637fcadd5
git <82054121+KXzeno@users.noreply.github.com>
2024-12-05 01:50:52 -0800

    fix: rectify static typing for page props

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,25 +1,28 @@
 import { ResolvingMetadata } from "next";
 
 export async function generateMetadata(
-  { params, searchParams }: { params: { slug: string }, searchParams: object }, 
+  { params, searchParams }: { params: Promise<{ slug: string }>, searchParams: object }, 
   parent: ResolvingMetadata) {
     // read route params
-    const { slug } = params;
+    const slug = params.then(res => res.slug);
 
-    // fetch data
-    const post = await fetch(`https://blog.karnovah.com/posts/${slug}`).then((res) => res.json());
+    if (typeof slug === "string") {
 
-      // optionally access and extend (rather than replace) parent metadata
-      // const previousImages = (await parent).openGraph?.images || []
+      // fetch data
+      const post = await fetch(`https://blog.karnovah.com/posts/${slug}`).then((res) => res.json());
 
-      return {
-      title: `${post.title}`,
-      /*
-         openGraph: {
-         images: ['/path/to/img', ...previousImages],
-         },
-       */
+        // optionally access and extend (rather than replace) parent metadata
+        // const previousImages = (await parent).openGraph?.images || []
+
+        return {
+        title: `${post.title}`,
+        /*
+           openGraph: {
+           images: ['/path/to/img', ...previousImages],
+           },
+         */
+      }
     }
-  }
 
+  }
   export default function Page({ params, searchParams }: { params: object, searchParams: object }) {}
