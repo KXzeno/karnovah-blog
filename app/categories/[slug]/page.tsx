ea5fc51f7944825e33669f72ab63dc8d43e474be
git <82054121+KXzeno@users.noreply.github.com>
2024-12-05 01:55:47 -0800

    fix: rectify typing and chain promises

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -3,23 +3,25 @@ import { notFound } from 'next/navigation';
 import { getCategory } from '@A/PostActions';
 import PostsInCategory from '@M/PostsInCategory';
 
-export default async function Category({ params }: { params: { slug: string }}): Promise<React.ReactNode> {
-  let slug = params.slug.toLowerCase();
-  let cat = await getCategory(slug);
-  if (cat === null || cat === undefined) {
-    // No need for return keyword; returns a 'never' type
-    return notFound();
-  }
+export default async function Category({ params }: { params: Promise<{ slug: string }>}): Promise<React.ReactNode> {
+  let slug = params.then(params => params.slug).then(slug => slug.toLowerCase());
+  if (typeof slug === "string") {
+    let cat = await getCategory(slug);
+    if (cat === null || cat === undefined) {
+      // No need for return keyword; returns a 'never' type
+      return notFound();
+    }
 
-  /** @privateRemarks
-   * Brief moments where cat returns null befores serve,
-   *  wrapping the component with suspense, I determined, will use
-   *  existing fallbacks, if not loading.tsx then not-found.tsx. 
-   */
-  return (
-    <React.Suspense>
-      <PostsInCategory category={cat} />
-    </React.Suspense>
-  );
+    /** @privateRemarks
+     * Brief moments where cat returns null befores serve,
+     *  wrapping the component with suspense, I determined, will use
+     *  existing fallbacks, if not loading.tsx then not-found.tsx. 
+     */
+    return (
+      <React.Suspense>
+        <PostsInCategory category={cat} />
+      </React.Suspense>
+    );
+  }
 }
 
