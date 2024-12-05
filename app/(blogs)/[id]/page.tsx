e37ebcf2445b20782b9bafba872dc2223478303e
git <82054121+KXzeno@users.noreply.github.com>
2024-12-05 02:56:49 -0800

    Remove fallback

--- app/(blogs)/[id]/page.tsx
+++ app/(blogs)/[id]/page.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import { Post } from '@/components';
 import './not-found';
-import { notFound } from 'next/navigation';
 
 type PageProps = Promise<{ id: string }>;
 
 export default async function Blog(params: PageProps): Promise<React.ReactNode> {
   let { id } = await params;
-  if (typeof id === "string") {
-    return (
-      <>
-        <Post param={id}/>
-      </>
-    );
-  } else {
-    notFound();
-  }
-}
+  return (
+    <>
+      <Post param={id}/>
+    </>
+  );
+} 
