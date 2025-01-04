8627fe62f5dabf5e973b0712fa6f17e9ff1ca2c3
git <82054121+KXzeno@users.noreply.github.com>
2025-01-04 13:33:01 -0800

    docs: elucidate control flow

--- app/components/MOB/CodeBox.tsx
+++ app/components/MOB/CodeBox.tsx
@@ -33,29 +33,37 @@ enum Lang {
 
     for (let i = 0; i < (children as React.ReactElement[]).length; i++) {
       let { children: content, className: classes }: { children: string, className: string } = children[i].props;
+      // Create RegExp array of matches
       let matches: RegExpStringIterator<RegExpExecArray> = content?.matchAll(LuaRgx.Reserved);
       if (matches) {
+        // Prepare final node transformation
         let carrier: React.ReactElement[] = [];
+        // Split html content by match
         let fragmented: string[] = content.split(LuaRgx.Reserved);
         for (let match of matches) {
+          // If index 0 contains content, append it to carrier node
           while (fragmented[0].length !== 0) {
             carrier.push(<>{fragmented.shift()}</>);
             fragmented.shift();
           }
+          // If index 0 does not contain content, it is a keyword
+          // Map it as a styled span element and append to carrier node
           carrier.push(<span key={fragmented.length} className='text-purple-400'>{match[0]}</span>);
           fragmented.shift();
+          // Ensure iterable deletion
           matches.drop(1);
         }
+        // Append all unchecked content to carrier node
         while (fragmented.length !== 0) {
           carrier.push(<>{fragmented.shift()}</>);
         }
-        console.log(carrier);
-        let newNode: React.ReactNode = 
-          <code className='text-inherit font-dosis'> {...carrier}</code>
-          termChildren.push(<span className='px-2 select-none'>{i + 1}</span>);
-          termChildren.push(newNode);
-          continue;
+        let newNode: React.ReactNode = <code className='text-inherit font-dosis'> {...carrier}</code>
+        // Create auto-index on mapped node
+        termChildren.push(<span className='px-2 select-none'>{i + 1}</span>);
+        termChildren.push(newNode);
+        continue;
       }
+      // Create auto-index
       termChildren.push(<span className='px-2 select-none'>{i + 1}</span>);
       termChildren.push(children[i]);
     }
