2a21186866ca56b65e0a40db8c06b989653a599f
git <82054121+KXzeno@users.noreply.github.com>
2025-01-06 19:52:08 -0800

    feat: hoist data query to dynamic route

--- app/(blogs)/[id]/page.tsx
+++ app/(blogs)/[id]/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Metadata, ResolvingMetadata } from 'next';
+
 import { Post } from '@/components';
+import { readPost } from '@A/PostActions';
 import './not-found';
 
 type Params = Promise<{ id: string }>;
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>
+
+let data: Awaited<ReturnType<typeof readPost>>;
 
 export async function generateMetadata(props: {
   params: Params,
@@ -11,13 +16,26 @@ export async function generateMetadata(props: {
 }) {
   let params = await props.params;
   let id = params.id;
+
+  // Is it odd that data is queried during metadata 
+  // definition instead of the dynamic route?
+  let postData = await readPost(id);
+  data = postData;
+
+  return {
+    title: `${postData ? postData.title : ''}`,
+    openGraph: {
+      title: `${postData ? postData.title : ''}`,
+      description: `${postData ? postData.description : ''}`
+    }
+  }
 };
 
 export default async function Blog(props: { params: Params, searchParams: SearchParams }): Promise<React.ReactNode> {
-  let { id } = await props.params;
+  // let { id } = await props.params;
   return (
     <>
-      <Post param={id}/>
+      <Post post={data} />
     </>
   );
 } 
