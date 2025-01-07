9743d133c21819737860720761e94284de5bd343
git <82054121+KXzeno@users.noreply.github.com>
2025-01-07 00:09:05 -0800

    fix: map link to proper route identifier

--- app/components/MOB/PostsInCategory.tsx
+++ app/components/MOB/PostsInCategory.tsx
@@ -13,11 +13,12 @@ export default function PostsInCategory({ category }: { category: unknown }) {
           posts.reverse().map(post => {
             let title = post.title;
             let date = post.createdAt.toISOString().split('T')[0];
+            let targetRoute = title.toLowerCase().split(/\s/).join('-');
             return (
               <li key={title}>
                 <time>{`${date}`}</time>
                 <span>—</span>
-                <Link href={`/${title.toLowerCase()}`}>{`${title}`}</Link>
+                <Link href={`/${targetRoute}`}>{`${title}`}</Link>
               </li>
             )
           })}
