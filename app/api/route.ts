1714341dbebc70ae341543ea003b7e0eff8f78bb
git <82054121+KXzeno@users.noreply.github.com>
2025-01-14 00:56:18 -0800

    feat: use `revalidatePath` for cache updates

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,16 +1,17 @@
 import { type NextRequest } from "next/server";
-import { revalidatePath } from "next/cache";
+import { revalidateTag } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function GET(req: NextRequest) {
-  let headers = new Headers(req.headers);
+  // let headers = new Headers(req.headers);
   let params = req.nextUrl.searchParams;
   for (let [q, val] of params.entries()) {
     if (q === 'rvc' && val === '1') {
-      revalidatePath('/');
+      revalidateTag('posts');
       console.log('Revalidated cache.');
       redirect('/');
     }
+    params.delete(q);
     console.log(`Query: ${q}\nValue: ${val}\n\n`);
   }
 }
