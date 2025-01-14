ef7ff9aff5a2f2be75f48462f939bbc754328211
git <82054121+KXzeno@users.noreply.github.com>
2025-01-14 01:12:11 -0800

    fix: hoist `unstable_cache`

--- app/components/MOB/Landing.tsx
+++ app/components/MOB/Landing.tsx
@@ -21,50 +21,50 @@ interface Post {
 // Seems to only update if the site is republished
 // Robust fix: compare updated date and creation date
 
-export default async function Landing() {
-  let getCachedPosts = unstable_cache(
-    async () => {
-      return await readPostAll({ orderBy: { createdAt: 'desc' } });
-    }, ['posts'], { tags: ['posts'] }
-  );
-  // let initialData: Post[] | null = await readPostAll({ orderBy: { createdAt: 'desc' } }) ?? null;
-  /** @remarks
-   * Unstable cache will be "use cache" in the future
-   *
-   * Stringify's ISO Dates, thus additional safeguards for 
-   * type mapping logic should be handled from the Feed RFC
-   */
-  let initialData = await getCachedPosts() ?? null;
-  let initialCursor: number | null = null;
-  // revalidatePath('/');
-  if (!initialData || initialData.length <= 0) {
-    initialCursor = null;
-    initialData = [{
-      post_id: 0,
-      title: 'Zero Query',
-      createdAt: new Date(),
-      published: true,
-      subtitle: 'No posts have queried',
-      description: 'If you see this, either there are no posts to be queried from the database or none of them are marked published.',
-      choice: 2 
-    }] as Post[];
-    console.log('Query for posts failed.');
-  } else {
-    initialCursor = initialData[initialData.length - 1].post_id;
+/** @remarks
+ * Unstable cache will be "use cache" in the future
+ *
+ * Stringify's ISO Dates, thus additional safeguards for 
+ * type mapping logic should be handled from the Feed RFC
+ */
+let getCachedPosts = unstable_cache(
+  async () => {
+    return await readPostAll({ orderBy: { createdAt: 'desc' } });
+  }, ['posts'], { tags: ['posts'] });
+
+  export default async function Landing() {
+    // let initialData: Post[] | null = await readPostAll({ orderBy: { createdAt: 'desc' } }) ?? null;
+    let initialData = await getCachedPosts() ?? null;
+    let initialCursor: number | null = null;
+    // revalidatePath('/');
+    if (!initialData || initialData.length <= 0) {
+      initialCursor = null;
+      initialData = [{
+        post_id: 0,
+        title: 'Zero Query',
+        createdAt: new Date(),
+        published: true,
+        subtitle: 'No posts have queried',
+        description: 'If you see this, either there are no posts to be queried from the database or none of them are marked published.',
+        choice: 2 
+      }] as Post[];
+      console.log('Query for posts failed.');
+    } else {
+      initialCursor = initialData[initialData.length - 1].post_id;
+    }
+
+
+    return (
+      initialCursor && initialData ?
+        <>
+          <React.Suspense>
+            <Feed initialData={initialData} initialCursor={initialCursor}/>
+          </React.Suspense>
+        </> :
+        <>
+          <React.Suspense>
+            <Feed initialData={initialData} initialCursor={0} />
+          </React.Suspense>
+        </>
+    )
   }
-
-
-  return (
-    initialCursor && initialData ?
-      <>
-        <React.Suspense>
-          <Feed initialData={initialData} initialCursor={initialCursor}/>
-        </React.Suspense>
-      </> :
-      <>
-        <React.Suspense>
-          <Feed initialData={initialData} initialCursor={0} />
-        </React.Suspense>
-      </>
-  )
-}
