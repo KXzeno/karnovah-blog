e35882ae9d1d1c750191a252a6a56f87e82eeeb7
git <82054121+KXzeno@users.noreply.github.com>
2025-01-19 05:33:33 -0800

    feat: implement simple loading UI

---
+++ app/(blogs)/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import { Loader } from "@M/Icons";
+
+export default function Loading() {
+  return (
+    <div className='min-h-screen min-w-screen content-center justify-center'>
+      <Loader size='lg' />
+    </div>
+  );
+}
