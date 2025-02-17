4fecc6a3fec34d6f9b2eb1bfb5a07b9452086b7f
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-02-17 09:25:22 -0800

    Rename module `FOB` -> `forward`

---
+++ app/components/forward/Authors/Author.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { readFile } from 'fs/promises';
+import Image from 'next/image';
+import './Authors.css';
+
+async function getFile(filePath: string) {
+  try {
+    let data = await readFile(`./app/${filePath}`, { encoding: 'utf8' });
+    let content = JSON.parse(data)
+    return content;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export default async function Author({ user, date }: { user: string, date: string }): Promise<React.ReactNode> {
+  let authors = await getFile('components/FOB/Authors/authorList.json');
+  if (!user || !authors) {
+    throw new Error('Invalid user');
+  }
+  console.log(authors[user]);
+
+  return (
+    <div id="author-profile">
+      <span id="publish-date">{`${date}`}</span>
+      <div id="author-name">
+          <span>{`${user}`}</span>
+          <Image src={`/${user}.png`} alt='author-img' width='32' height='32'/>
+      </div>
+    </div>
+  );
+}
---
+++ app/components/forward/SubMenu.tsx
@@ -0,0 +1,156 @@
+import React, {SetStateAction} from 'react';
+import useClickListener from '@H/use-click-listener';
+
+interface SubMenuProps {
+  children: React.ReactNode,
+  showSubMenu: boolean,
+  toggleSubMenu: Function,
+}
+
+/**
+ * Self-serving sub menu component
+ * @param {<>} children - HTML to be extraneously inputted
+ * @param {boolean} showSubMenu - represents truthiness on DOM node existence
+ * @param {function} toggleSubMenu - setter function for showSubMenu
+ * @param {object} delegated - Delegated props
+ * @returns {HTML} DOM node representing sub menu
+ * @author Kx
+ */
+export default React.forwardRef(function SubMenu({ 
+  children, 
+  showSubMenu, 
+  toggleSubMenu, 
+  ...delegated
+}: SubMenuProps, subMenuRef: React.LegacyRef<HTMLDivElement>) {
+  /**
+   * Represents condition for toggle state
+   * State behaving as either > 0 or === 0
+   * Defaults to '1' to avoid premature resolution
+   */
+  let [checkChange, setCheckChange] = React.useState(1);
+
+  // Represents custom hook that fires a click event listener
+  const [pointer, setPointer] = useClickListener();
+
+  // Represents current screen width
+  let [width, setWidth] = React.useState(window.innerWidth);
+
+  // Custom breakpoint variable using 'width' state
+  let isMobile = width <= 768;
+
+  /**
+   * Callback event handler to retain SubMenu state
+   * @returns {function} perennially sets checkChange
+   * @author Kx
+   */
+  function toggleEnter() {
+    setCheckChange((prev) => prev + 1);
+  }
+
+  /**
+   * Callback event handler to remove SubMenu toggle state
+   * @param {object} e - event used for comparison
+   * @returns {} Optionally resolves to N/A when state reaches SubMenu
+   * @return {function} Optionally remove SubMenu toggle state if on mobile
+   * @return {function} Sets checkChange to 0
+   * @author Kx
+   */
+  let toggleLeave = React.useCallback((e: Event) => {
+    let proxyRef = subMenuRef as React.RefObject<HTMLElement>;
+    if (subMenuRef && proxyRef.current && e.target as HTMLElement === proxyRef.current.firstChild) {
+      return;
+    };
+
+    if (isMobile) {
+      toggleSubMenu();
+    }
+
+    setCheckChange(0);
+  }, [isMobile, toggleSubMenu, subMenuRef]);
+
+  /**
+   * Callback event handler to read current window size
+   * @returns {function} 'width' setter function
+   * @author Kx
+   */
+  function handleWindowSizeChange(): void {
+    setWidth(window.innerWidth);
+  }
+
+  /**
+   * Onmount hook to identify window size
+   * @param {function} () - Handles resize events and updates 'width'
+   * @author Kx
+   */
+  React.useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    }
+  }, []);
+
+  /**
+   * Hook to conditionally display SubMenu
+   * @param {function} () - Conditionally applies specific event handlers based on breakpoint
+   * @author Kx
+   */
+  React.useEffect(() => {
+    if (!isMobile && subMenuRef) {
+      let smRef = (subMenuRef as React.RefObject<HTMLElement>).current;
+      smRef && smRef.addEventListener('pointermove', toggleEnter);
+      smRef && smRef.addEventListener('pointerleave', toggleLeave);
+
+      showSubMenu === true && pointer === true && checkChange !== 0
+        ? +(() => {
+          setPointer(false);
+          return;
+        })()
+          : +(() => {
+            if (checkChange === 0 && pointer === true && showSubMenu === true) {
+              toggleSubMenu();
+            }
+          })();
+          return () => {
+            smRef?.removeEventListener('pointermove', toggleEnter);
+            smRef?.removeEventListener('pointerleave', toggleLeave);
+          }
+    }
+
+    if (isMobile) {
+      // showSubMenu === true && isMobile && checkChange !== 0
+      let smRef: HTMLElement | null;
+      if (subMenuRef) {
+        smRef = (subMenuRef as React.RefObject<HTMLElement>).current;
+      } else {
+        return;
+      }
+
+      smRef && smRef.addEventListener('touchend', toggleEnter);
+      window.addEventListener('touchend', toggleLeave);
+
+      showSubMenu === true && checkChange !== 0
+        ? setCheckChange(1)
+        : toggleSubMenu();
+
+        return () => {
+          smRef?.removeEventListener('touchend', toggleEnter);
+          window.removeEventListener('touchend', toggleLeave);
+        }
+    }
+
+  }, [pointer, showSubMenu, toggleSubMenu, setPointer, subMenuRef, toggleLeave, checkChange, isMobile]);
+
+  return (
+    <div className="min-w-full absolute">
+      <div ref={subMenuRef} className="pointer-events-auto w-16 h-16 mx-auto">
+        <nav className="grid grid-cols-2 mx-auto absolute bg-white text-black w-48 h-min max-h-80 top-16 justify-self-center inset-0 left-[4rem] text-center text-sm place-items-center rounded-[0.7rem] p-2 -translate-y-[5.3rem] z-[1]">
+          {children}
+        </nav>
+      </div>
+      <div className="grid grid-cols-1 justify-items-center">
+        <div className="z-10 bg-white h-4 w-4 origin-center rotate-45 rounded-tl-[0.1rem] -translate-y-[5.7rem] translate-x-[2.23rem]">
+        </div>
+      </div>
+    </div>
+  );
+})
